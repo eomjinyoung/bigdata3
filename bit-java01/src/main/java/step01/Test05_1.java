3ae124aea43dbd34dc949b662d0c65bf0a2a7794
jinyoung <jinyoung.eom@gmail.com>
2017-07-04 15:42:36 +0900

    .

---
+++ bit-java01/src/main/java/step01/Test05_1.java
@@ -0,0 +1,15 @@
+/* 문자 리터럴
+ * => 작은 따옴표('')를 사용하여 한 문자를 표현한다.
+ *    작은 따옴표 안에 여러 문자를 기술해서는 안된다.
+ */
+package step01;
+
+public class Test05_1 {
+  public static void main(String[] args) {
+    System.out.println('A'); 
+    System.out.println('가');
+    System.out.println('AB'); // 컴파일 오류!
+  }
+
+}
+ 
