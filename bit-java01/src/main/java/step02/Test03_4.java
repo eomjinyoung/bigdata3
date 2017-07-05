441424ca5ffaeb48ea710d7fbf4ac4fd95c449cb
jinyoung <jinyoung.eom@gmail.com>
2017-07-05 14:55:20 +0900

    .

---
+++ bit-java01/src/main/java/step02/Test03_4.java
@@ -0,0 +1,18 @@
+/* 자바는 정수의 기본 크기로 4바이트를 사용한다.
+ * => 특별한 경우가 아니면, 가능한 int 변수를 사용하라!
+ * 
+ */
+package step02;
+
+public class Test03_3 {
+  public static void main(String[] args) {
+    byte kor = 89;
+    byte bonus = 5;
+
+    byte kor2 = kor + 5;
+
+
+  }
+
+}
+ 
