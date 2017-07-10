79c103deb031492c39dd7f1ab302eacd1b40c68e
jinyoung <jinyoung.eom@gmail.com>
2017-07-10 12:40:33 +0900

    .

--- bit-java01/src/main/java/step05/Test05_3.java
+++ bit-java01/src/main/java/step05/Test05_3.java
@@ -1,4 +1,4 @@
-/* 명령어의 실행 흐름 제어: 반복문 for II - 변수선언문 생략
+/* 명령어의 실행 흐름 제어: 반복문 for III - 증가/감소 생략
  * => 
  */
 package step05;
@@ -7,8 +7,10 @@ public class Test05_3 {
 
   public static void main(String[] args) {
     int a = 1;
-    for (; a <= 10; a++)
+    for (; a <= 10;) {
       System.out.println(a);
+      a++;
+    }
   } 
 
 }
