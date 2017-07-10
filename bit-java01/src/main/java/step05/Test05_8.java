cde8097a9de8d17cc525cc30d23d2843e8f01604
jinyoung <jinyoung.eom@gmail.com>
2017-07-10 12:44:50 +0900

    .

--- bit-java01/src/main/java/step05/Test05_8.java
+++ bit-java01/src/main/java/step05/Test05_8.java
@@ -1,27 +1,26 @@
-/* 명령어의 실행 흐름 제어: 반복문 for 와 배열 II
- * => 배열을 위한 특별 for 문법
- *    for (배열의 값을 담을 변수 선언 : 배열) 
+/* 명령어의 실행 흐름 제어: 반복문 for 와 Collection 객체
+ * => 배열 뿐만아니라 Collection 객체도 가능하다.
+ *    for (컬렉션에서 꺼낸 값을 담을 변수 선언 : 컬렉션객체) 
  *      명령문;
- * => 배열 전체를 반복할 때 유용하다.
- *    단 배열의 일부를 반복할 수는 없다.
+ * => 컬렉션 전체를 반복할 때 유용하다.
+ *    단 컬렉션의 일부를 반복할 수는 없다.
  */
 package step05;
+
+import java.util.ArrayList;
 
 public class Test05_8 {
 
   public static void main(String[] args) {
-    int[] arr1 = {10, 20, 30, 40, 50, 60, 70};
+    ArrayList<String> nameList = new ArrayList<>();
+    nameList.add("홍길동");
+    nameList.add("임꺽정");
+    nameList.add("유관순");
+    nameList.add("안중근");
+    nameList.add("윤봉길");
     
-    for (int value : arr1) { 
+    for (String value : nameList) { 
       System.out.println(value);
-    }
-    
-    System.out.println("----------------------");
-    
-    String[] names = {"홍길동", "임꺽정", "유관순", "안중근", "윤봉길"};
-    
-    for (String name : names) {
-      System.out.println(name);
     }
   } 
 
