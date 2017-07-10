237ff83ecedd250d5c9fbc1a6cffd8b6df78d14f
jinyoung <jinyoung.eom@gmail.com>
2017-07-10 10:39:30 +0900

    .

---
+++ bit-java01/src/main/java/step05/Test05_1.java
@@ -0,0 +1,30 @@
+/* 명령어의 실행 흐름 제어: 반복문 for 
+ * => 초기 값이 조건에 부합하는 동안에는 계속 명령문 실행을 반복한다. 
+ * => 배열처럼 인덱스를 다루는 경우 for 문을 자주 사용한다.
+ * => 문법
+ *    for (변수선언; 조건; 증가/감소)
+ *      명령문;
+ *    for (변수선언; 조건; 증가/감소) {
+ *      명령문;
+ *      명령문;
+ *    }
+ */
+package step05;
+
+public class Test05_1 {
+
+  public static void main(String[] args) {
+    for (int a = 1; a <= 10; a++)
+      System.out.println(a);
+  } 
+
+}
+
+
+
+
+
+
+
+
+
