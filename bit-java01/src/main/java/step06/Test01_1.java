75f6f01ff88325d2add5acc99c7f352b0d833135
jinyoung <jinyoung.eom@gmail.com>
2017-07-10 14:27:25 +0900

    .

--- bit-java01/src/main/java/step06/Test01_1.java
+++ bit-java01/src/main/java/step06/Test01_1.java
@@ -42,6 +42,76 @@ public class Test01_1 {
     }
     
     System.out.println("---------------------------");
+    
+    dan = 32;
+    
+    System.out.printf("[%d 단]\n", dan);
+    
+    for (int i = 1; i <= 9; i++) {
+      System.out.printf("%d * %d = %d\n", dan, i, dan * i);
+    }
+    
+    System.out.println("---------------------------");
+    
+    dan = 33;
+    
+    System.out.printf("[%d 단]\n", dan);
+    
+    for (int i = 1; i <= 9; i++) {
+      System.out.printf("%d * %d = %d\n", dan, i, dan * i);
+    }
+    
+    System.out.println("---------------------------");
+    
+    dan = 34;
+    
+    System.out.printf("[%d 단]\n", dan);
+    
+    for (int i = 1; i <= 9; i++) {
+      System.out.printf("%d * %d = %d\n", dan, i, dan * i);
+    }
+    
+    System.out.println("---------------------------");  
+
+    dan = 35;
+    
+    System.out.printf("[%d 단]\n", dan);
+    
+    for (int i = 1; i <= 9; i++) {
+      System.out.printf("%d * %d = %d\n", dan, i, dan * i);
+    }
+    
+    System.out.println("---------------------------");
+    
+    dan = 39;
+    
+    System.out.printf("[%d 단]\n", dan);
+    
+    for (int i = 1; i <= 9; i++) {
+      System.out.printf("%d * %d = %d\n", dan, i, dan * i);
+    }
+    
+    System.out.println("---------------------------");    
+    
+    dan = 40;
+    
+    System.out.printf("[%d 단]\n", dan);
+    
+    for (int i = 1; i <= 9; i++) {
+      System.out.printf("%d * %d = %d\n", dan, i, dan * i);
+    }
+    
+    System.out.println("---------------------------");
+    
+    dan = 41;
+    
+    System.out.printf("[%d 단]\n", dan);
+    
+    for (int i = 1; i <= 9; i++) {
+      System.out.printf("%d * %d = %d\n", dan, i, dan * i);
+    }
+    
+    System.out.println("---------------------------");
   }
 
 }
