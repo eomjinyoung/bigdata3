ad0dd8eb68b3dc4521e7e1fb0b321b1c78784022
jinyoung <jinyoung.eom@gmail.com>
2017-07-11 15:29:00 +0900

    .

--- bit-java01/src/main/java/step07/ex1/CalculatorTest.java
+++ bit-java01/src/main/java/step07/ex1/CalculatorTest.java
@@ -1,27 +1,41 @@
-/* 클래스 문법의 용도: 유지보수 하기 좋게 관련된 메서드를 묶을 때
- * 1) 사용자 정의 데이터 타입을 만들 때
- *    예) class Score {...}
- * 2) 유지보수 하기 좋게 관련된 메서드를 묶을 때
+/* 클래스 : 스태틱 메서드
  */
 package step07.ex1;
 
 public class CalculatorTest {
 
+  static float plus(float a, float b) {
+    return a + b;
+  }
+  
+  static float minus(float a, float b) {
+    return a - b;
+  }
+  
+  static float multiple(float a, float b) {
+    return a * b;
+  }
+  
+  static float divide(float a, float b) {
+    return a / b;
+  }
+  
   public static void main(String[] args) {
     /* 계산기 기능을 구현하면서 클래스 문법을 활용하는 방법을 익혀보자!
-     * 1단계: 결과 값에 대해 계속 연산을 수행한다. 
+     * 1단계: +, -, *, / 연산을 수행하는 메서드를 사용하여 다음 계산을 수행하라
      */
-    
-    float result = 0f; // 결과를 보관할 변수
-    
     // 계산식: 2 + 3 - 6 * 2 + 7 / 2 = 2.5
     // 단 연산자 우선순위는 무시하고 앞에서 부터 계속 계산하라!
-    result += 2;
-    result += 3;
-    result -= 6;
-    result *= 2;
-    result += 7;
-    result /= 2;
+    
+    // 계산 결과를 보관하기 위해 직접 로컬 변수를 준비한다.
+    float result = 0f;
+    
+    result = plus(result, 2);
+    result = plus(result, 3);
+    result = minus(result, 6);
+    result = multiple(result, 2);
+    result = plus(result, 7);
+    result = divide(result, 2);
     
     System.out.println(result);
     
--- bit-java01/src/main/java/step07/ex2/CalculatorTest.java
+++ bit-java01/src/main/java/step07/ex2/CalculatorTest.java
@@ -1,44 +1,53 @@
-/* 클래스 문법의 용도: 유지보수 하기 좋게 관련된 메서드를 묶을 때
- * 1) 사용자 정의 데이터 타입을 만들 때
- *    예) class Score {...}
- * 2) 유지보수 하기 좋게 관련된 메서드를 묶을 때
+/* 클래스 : 클래스 변수(class variable)
+ * => 클래스의 모든 메서드에서 공유하는 변수
+ * => 클래스 블록 안에 직접 변수를 선언한다.
+ *    그리고 변수 선언 앞에 static을 붙인다.
+ * => 클래스가 로딩될 때 Method Area라는 메모리 영역에 만들어지는 변수이다.
+ *    JVM이 종료될 때까지 유지된다.
+ *     
  */
 package step07.ex2;
 
 public class CalculatorTest {
 
-  static float plus(float a, float b) {
-    return a + b;
+  static float result = 0f; 
+
+  // 계산 결과는 result라는 클래스 변수에 저장하면 되기 때문에
+  // 계산 결과를 리턴할 필요가 없다.
+  // 그리고 파라미터로 계산할 값만 받으면 된다.
+  static void plus(float b) {
+    result += b; // result = result + b;
   }
   
-  static float minus(float a, float b) {
-    return a - b;
+  static void minus(float b) {
+    result -= b; // result = result - b;
   }
   
-  static float multiple(float a, float b) {
-    return a * b;
+  static void multiple(float b) {
+    result *= b; // result = result * b;
   }
   
-  static float divide(float a, float b) {
-    return a / b;
+  static void divide(float b) {
+    result /= b; // result = result / b;
   }
   
   public static void main(String[] args) {
     /* 계산기 기능을 구현하면서 클래스 문법을 활용하는 방법을 익혀보자!
-     * 2단계: +, -, *, / 연산을 별도의 메서드로 분리하라!
+     * 2단계: plus(), minus(), multiple(), divide()의 계산 결과를 저장할 변수를 
+     *       모든 메서드에서 직접 이용할 수 있도록 "클래스 변수(class variable)"로 만들어라!
+     *       main() 메서드의 로컬 변수로 저장하는 것 보다 관리하기 편하다!
      */
     
-    float result = 0f; // 결과를 보관할 변수
     
     // 계산식: 2 + 3 - 6 * 2 + 7 / 2 = 2.5
     // 단 연산자 우선순위는 무시하고 앞에서 부터 계속 계산하라!
     
-    result = plus(result, 2);
-    result = plus(result, 3);
-    result = minus(result, 6);
-    result = multiple(result, 2);
-    result = plus(result, 7);
-    result = divide(result, 2);
+    plus(2);
+    plus(3);
+    minus(6);
+    multiple(2);
+    plus(7);
+    divide(2);
     
     System.out.println(result);
     
