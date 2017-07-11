d2aed16c9456213ba30a949f3df2d85a07a736f5
jinyoung <jinyoung.eom@gmail.com>
2017-07-11 16:30:50 +0900

    .

--- bit-java01/src/main/java/step07/ex4/CalculatorTest.java
+++ bit-java01/src/main/java/step07/ex4/CalculatorTest.java
@@ -11,15 +11,16 @@ public class CalculatorTest {
 
   public static void main(String[] args) {
     /* 계산기 기능을 구현하면서 클래스 문법을 활용하는 방법을 익혀보자!
-     * 3단계: plus(), minus(), multiple(), divide()와 
-     *       이 메서드들이 사용하는 result 변수를 별도의 클래스로 분리한다.
+     * 4단계: 계산 결과를 개별적으로 관리하고 싶다면, 
+     *       클래스 변수 대신 인스턴스 변수로 만들라!
      */
     
-    // 계산식: 2 + 3 - 6 * 2 + 7 / 2 = 2.5
+    // 계산식1: 2 + 3 - 6 * 2 + 7 / 2 = 2.5
+    // 계산식2: 5 * 2 - 3 + 14 / 3 = 7
     // 단 연산자 우선순위는 무시하고 앞에서 부터 계속 계산하라!
     
-    // plus(), minus(), multiple(), divide()를 별도의 클래스로 묶어 두었기 때문에
-    // 사용하려면 반드시 클래스 이름을 명시해야 한다.
+    // 클래스 변수를 사용하면 두 개의 계산식을 동시에 진행할 수 없다.
+    // 왜? 모든 메서드가 한 개의 클래스 변수 "result"를 공유하기 때문이다.
     Calculator.plus(2);
     Calculator.plus(3);
     Calculator.minus(6);
@@ -27,6 +28,14 @@ public class CalculatorTest {
     Calculator.plus(7);
     Calculator.divide(2);
     
+    // 안타깝게도 기존 값에 이어서 계산을 수행할 것이다.
+    Calculator.plus(5);
+    Calculator.multiple(2);
+    Calculator.minus(3);
+    Calculator.plus(14);
+    Calculator.divide(3);
+    
+    // 그래서 각각의 계산식의 결과를 구분할 수 없다.
     System.out.println(Calculator.result);
     
   }
