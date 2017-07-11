ba34574ffc40f8941c796218c6906651c98757f9
jinyoung <jinyoung.eom@gmail.com>
2017-07-11 16:14:20 +0900

    .

--- bit-java01/src/main/java/step07/ex3/Calculator.java
+++ bit-java01/src/main/java/step07/ex3/Calculator.java
@@ -2,19 +2,22 @@ package step07.ex3;
 
 public class Calculator {
   
-  static float plus(float a, float b) {
-    return a + b;
+  static float result = 0f; 
+
+  static void plus(float b) {
+    result += b; // result = result + b;
   }
   
-  static float minus(float a, float b) {
-    return a - b;
+  static void minus(float b) {
+    result -= b; // result = result - b;
   }
   
-  static float multiple(float a, float b) {
-    return a * b;
+  static void multiple(float b) {
+    result *= b; // result = result * b;
   }
   
-  static float divide(float a, float b) {
-    return a / b;
+  static void divide(float b) {
+    result /= b; // result = result / b;
   }
+  
 }
--- bit-java01/src/main/java/step07/ex4/Calculator.java
+++ bit-java01/src/main/java/step07/ex4/Calculator.java
@@ -1,26 +1,23 @@
 package step07.ex4;
 
 public class Calculator {
-  // plus(), minus(), multiple(), divide() 메서드에서 사용할 변수도
-  // 이 클래스에 둔다면 코드 관리가 수월할 것이다.
-  // 이때 변수 선언 앞에 static을 붙여라!
-  static float result = 0f; // 결과를 보관할 변수
   
-  // result 변수가 같은 클래스에 있기 때문에 
-  // 결과를 리턴할 필요가 없다. 바로 result 변수에 저장하면 된다.
-  static void plus(float value) {
-    result += value;
+  static float result = 0f; 
+
+  static void plus(float b) {
+    result += b; // result = result + b;
   }
   
-  static void minus(float value) {
-    result -= value;
+  static void minus(float b) {
+    result -= b; // result = result - b;
   }
   
-  static void multiple(float value) {
-    result *= value;
+  static void multiple(float b) {
+    result *= b; // result = result * b;
   }
   
-  static void divide(float value) {
-    result /= value;
+  static void divide(float b) {
+    result /= b; // result = result / b;
   }
+  
 }
