75980b77e6b52c8d4cd36835ce2707a3396ed91f
jinyoung <jinyoung.eom@gmail.com>
2017-07-11 14:47:49 +0900

    .

--- bit-java01/src/main/java/step07/ex4/Calculator.java
+++ bit-java01/src/main/java/step07/ex4/Calculator.java
@@ -1,20 +1,26 @@
 package step07.ex4;
 
 public class Calculator {
+  // plus(), minus(), multiple(), divide() 메서드에서 사용할 변수도
+  // 이 클래스에 둔다면 코드 관리가 수월할 것이다.
+  // 이때 변수 선언 앞에 static을 붙여라!
+  static float result = 0f; // 결과를 보관할 변수
   
-  static float plus(float a, float b) {
-    return a + b;
+  // result 변수가 같은 클래스에 있기 때문에 
+  // 결과를 리턴할 필요가 없다. 바로 result 변수에 저장하면 된다.
+  static void plus(float value) {
+    result += value;
   }
   
-  static float minus(float a, float b) {
-    return a - b;
+  static void minus(float value) {
+    result -= value;
   }
   
-  static float multiple(float a, float b) {
-    return a * b;
+  static void multiple(float value) {
+    result *= value;
   }
   
-  static float divide(float a, float b) {
-    return a / b;
+  static void divide(float value) {
+    result /= value;
   }
 }
