acb48dfcce3e800283b021e45c573f2caa795942
jinyoung <jinyoung.eom@gmail.com>
2017-07-11 14:50:21 +0900

    .

--- bit-java01/src/main/java/step07/ex4/CalculatorTest.java
+++ bit-java01/src/main/java/step07/ex4/CalculatorTest.java
@@ -13,21 +13,20 @@ public class CalculatorTest {
      *       그 메서드가 있는 클래스에 두는 것이 유지보수에 좋다.
      */
     
-    
-    
     // 계산식: 2 + 3 - 6 * 2 + 7 / 2 = 2.5
     // 단 연산자 우선순위는 무시하고 앞에서 부터 계속 계산하라!
     
-    // 다른 클래스에 묶은 메서드를 호출할 때는
-    // 메서드 이름 앞에 클래스 이름을 지정해야 한다.
-    result = Calculator.plus(result, 2);
-    result = Calculator.plus(result, 3);
-    result = Calculator.minus(result, 6);
-    result = Calculator.multiple(result, 2);
-    result = Calculator.plus(result, 7);
-    result = Calculator.divide(result, 2);
+    // 이제 계산 결과는 Calculator 클래스에 보관되기 때문에 
+    // 여기에서는 따로 계산 결과를 관리할 필요가 없다.
+    // 코드 관리가 쉬워진다.
+    Calculator.plus(2);
+    Calculator.plus(3);
+    Calculator.minus(6);
+    Calculator.multiple(2);
+    Calculator.plus(7);
+    Calculator.divide(2);
     
-    System.out.println(result);
+    System.out.println(Calculator.result);
     
   }
 
