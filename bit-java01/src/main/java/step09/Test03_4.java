5019b728f6e48f3638695f8a1a9d231558d12395
jinyoung <jinyoung.eom@gmail.com>
2017-07-12 16:14:34 +0900

    .

--- bit-java01/src/main/java/step09/Test03_4.java
+++ bit-java01/src/main/java/step09/Test03_4.java
@@ -4,29 +4,29 @@
  */
 package step09;
 
-class MyClassX {
-  static int x;
+class X {
+  static int value;
   static {
-    System.out.println("<MyClassX>");
-    MyClassY.y = 200;
-    x = 100;
-    System.out.println("</MyClassX>");
+    System.out.println("<X>");
+    Y.value = 100;
+    value = 100;
+    System.out.println("</X>");
   }
 }
 
-class MyClassY {
-  static int y;
+class Y {
+  static int value;
   static {
-    System.out.println("<MyClassY>");
-    MyClassX.x = 200;
-    y = 200;
-    System.out.println("</MyClassY>");
+    System.out.println("<Y>");
+    X.value = 200;
+    value = 200;
+    System.out.println("</Y>");
   }
 }
 
 public class Test03_4 {
   public static void main(String[] args) {
-    System.out.println(MyClassX.x);
+    System.out.println(X.value);
     
   }
 }
