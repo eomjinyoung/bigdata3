48afc94400c174a59b3c765381e47efd51c1b382
jinyoung <jinyoung.eom@gmail.com>
2017-07-13 10:49:01 +0900

    .

--- bit-java01/src/main/java/step10/Test02_3.java
+++ bit-java01/src/main/java/step10/Test02_3.java
@@ -1,6 +1,7 @@
 /* 인스턴스 멤버 : 생성자(constructor)와 일반 인스턴스 메서드 II
- * => 생성자는 일반 인스턴스 메서드와 달리 딱 한 번만 호출할 수 있다.
+ * => 생성자는 일반 인스턴스 메서드와 달리
  *    인스턴스를 생성할 때 딱 한 번만 호출할 수 있다.
+ * => 레퍼런스를 이용하여 따로 호출할 수 없다.
  *   
  */
 package step10;
@@ -17,7 +18,7 @@ class MyClass6 {
 public class Test02_3 {
   public static void main(String[] args) {
     MyClass6 obj = new MyClass6();
-    //obj.MyClass5();
+    //obj.MyClass6(); // 인스턴스 레퍼런스를 사용하여 따로 생성자를 호출할 수 없다!
   }
 }
 
