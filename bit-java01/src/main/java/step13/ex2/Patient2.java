a8d19e98e33402f4a5e67463f1ea5191b86f9808
jinyoung <jinyoung.eom@gmail.com>
2017-07-17 10:41:18 +0900

    .

---
+++ bit-java01/src/main/java/step13/ex2/Patient2.java
@@ -0,0 +1,10 @@
+package step13.ex2;
+
+public class Patient2 {
+  // 레퍼런스로 직접 접근하지 못하도록 modifier를 붙인다.
+  private String name;
+  private int age;
+  private float weight;
+  private float height;
+  private boolean gender;
+}
