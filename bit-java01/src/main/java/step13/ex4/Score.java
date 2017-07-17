c75edb94e691c5ad34e66b57d4cfa41b5463cea2
jinyoung <jinyoung.eom@gmail.com>
2017-07-17 12:23:39 +0900

    .

--- bit-java01/src/main/java/step13/ex4/Score.java
+++ bit-java01/src/main/java/step13/ex4/Score.java
@@ -8,4 +8,75 @@ public class Score {
   private int sum;
   private float aver;
   
+  Score() {}
+  
+  Score(String name, int kor, int eng, int math) {
+    this.name = name;
+    this.kor = kor;
+    this.eng = eng;
+    this.math = math;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getKor() {
+    return kor;
+  }
+
+  public void setKor(int kor) {
+    if (kor < 0 || kor > 100)
+      return;
+    this.kor = kor;
+    compute();
+  }
+
+  public int getEng() {
+    return eng;
+  }
+
+  public void setEng(int eng) {
+    if (eng < 0 || eng > 100)
+      return;
+    this.eng = eng;
+    compute();
+  }
+
+  public int getMath() {
+    return math;
+  }
+
+  public void setMath(int math) {
+    if (math < 0 || math > 100)
+      return;
+    this.math = math;
+    compute();
+  }
+
+  public int getSum() {
+    return sum;
+  }
+
+  public float getAver() {
+    return aver;
+  }
+  
+  void compute() {
+    this.sum = this.kor + this.eng + this.math;
+    this.aver = this.sum / 3f;
+  }
+
 }
+
+
+
+
+
+
+
+
