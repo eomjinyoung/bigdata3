247514421841347fc79e584b14674999bf85d644
jinyoung <jinyoung.eom@gmail.com>
2017-07-17 12:31:00 +0900

    .

--- bit-java01/src/main/java/step13/ex4/Test01.java
+++ bit-java01/src/main/java/step13/ex4/Test01.java
@@ -3,7 +3,20 @@ package step13.ex4;
 public class Test01 {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    Score score = new Score("홍길동", 97, 88, 100);
+    
+    System.out.println(score.getSum());
+    System.out.println(score.getAver());
+    
+    // 그 클래스의 내부에서만 사용하는 메서드는 
+    // 다른 클래스가 사용할 수 없게 하는게 유지보수에 좋다!
+    //score.compute();
+    
+    System.out.println("--------------------");
+    
+    score.setKor(80);
+    System.out.println(score.getSum());
+    System.out.println(score.getAver());
 
   }
 
