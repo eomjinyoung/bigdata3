8a1eaa0a524d374283746d54685b601d6c279595
jinyoung <jinyoung.eom@gmail.com>
2017-07-17 12:35:21 +0900

    .

--- bit-java01/src/main/java/step13/ex4/Test01.java
+++ bit-java01/src/main/java/step13/ex4/Test01.java
@@ -1,3 +1,8 @@
+/* 캡슐화 적용
+ * => 변수 뿐만 아니라 메서드에 대해서도 private을 사용한다는 것을 잊지말라!
+ * => 변수이든 메서드이든 공개를 최소화시키는 게 유지보수에 좋다.
+ *    즉 클래스를 목적에 따라 제대로 사용하지 못하고 잘못 사용하는 것을 막을 수 있다. 
+ */
 package step13.ex4;
 
 public class Test01 {
@@ -15,6 +20,7 @@ public class Test01 {
     System.out.println("--------------------");
     
     score.setKor(80);
+    
     System.out.println(score.getSum());
     System.out.println(score.getAver());
 
