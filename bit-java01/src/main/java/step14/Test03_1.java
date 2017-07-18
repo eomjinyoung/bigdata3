d585ea141888015f5c3ebecc28c857ed1f986551
jinyoung <jinyoung.eom@gmail.com>
2017-07-18 14:30:01 +0900

    .

--- bit-java01/src/main/java/step14/Test03_1.java
+++ bit-java01/src/main/java/step14/Test03_1.java
@@ -1,5 +1,7 @@
 /* StringBuffer: mutable 객체
- * => String 클래스와는 달리 문자열을 변경할 수 있다.
+ * => String 클래스와 다른점
+ *    1) 문자열을 변경할 수 있다.
+ *    2) Object의 equals()를 오버라이딩 하지 않았기 때문에, 내용을 비교하지 못한다.
  */
 package step14;
 
@@ -11,7 +13,14 @@ public class Test03_1 {
     
     System.out.println(sb1);
     System.out.println(sb2);
-
+    
+    if (sb1.equals(sb2)) System.out.println("같다!");
+    else System.out.println("다르다!");
   }
 
 }
+
+
+
+
+
