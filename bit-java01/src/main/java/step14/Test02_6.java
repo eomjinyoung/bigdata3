66a4391b4176d5116b4013e71351dbff53468c91
jinyoung <jinyoung.eom@gmail.com>
2017-07-18 11:54:28 +0900

    .

--- bit-java01/src/main/java/step14/Test02_6.java
+++ bit-java01/src/main/java/step14/Test02_6.java
@@ -1,20 +1,22 @@
-/* String 클래스 - chatAt(), indexOf(), 
+/* String 클래스 - chatAt(), indexOf(), lastIndexOf()
  * => charAt(): 특정 위치의 문자를 추출하기
  * => indexOf(): 문자의 인덱스, 문자열의 시작 인덱스 
+ *    lastIndexOf(): indexOf()랑 같다. 다만 문자열의 뒤쪽에서부터 찾는다.
  */
 package step14;
 
 public class Test02_6 {
 
   public static void main(String[] args) {
-    String s1 = "abcdefghijklmn";
+    String s1 = "abcdefghicdefg";
     char ch = s1.charAt(4); // 인덱스는 0부터 시작한다.
     System.out.println(ch);
     System.out.println("-------------------------");
     
     int i = s1.indexOf('f'); // 5
     int i2 = s1.indexOf("cde"); // 2
-    System.out.printf("%d, %d\n", i, i2);
+    int i3 = s1.lastIndexOf("cde"); // 9
+    System.out.printf("%d, %d, %d\n", i, i2, i3);
     System.out.println("-------------------------");
     
     
