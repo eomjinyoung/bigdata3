fa06a13089af507fe7bf30a59a84ebab3f1e43cb
jinyoung <jinyoung.eom@gmail.com>
2017-07-18 11:57:28 +0900

    .

--- bit-java01/src/main/java/step14/Test02_6.java
+++ bit-java01/src/main/java/step14/Test02_6.java
@@ -1,7 +1,9 @@
-/* String 클래스 - chatAt(), indexOf(), lastIndexOf()
+/* String 클래스 - chatAt(), indexOf(), lastIndexOf(), startsWith(), endsWith()
  * => charAt(): 특정 위치의 문자를 추출하기
  * => indexOf(): 문자의 인덱스, 문자열의 시작 인덱스 
  *    lastIndexOf(): indexOf()랑 같다. 다만 문자열의 뒤쪽에서부터 찾는다.
+ * => startsWith(): 문자열이 특정 문자열로 시작하는지 여부를 조사한다.
+ * => endsWith(): 문자열이 특정 문자열로 끝나는지 여부를 조사한다.
  */
 package step14;
 
@@ -19,7 +21,9 @@ public class Test02_6 {
     System.out.printf("%d, %d, %d\n", i, i2, i3);
     System.out.println("-------------------------");
     
-    
+    String name = "James Dean";
+    if (name.startsWith("James")) System.out.println("OK!");
+    if (name.endsWith("Dean")) System.out.println("OK!");
   }
 
 }
