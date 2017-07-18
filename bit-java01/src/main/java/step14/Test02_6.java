8a93415ce24a21f8f093611f10987d83c72d8102
jinyoung <jinyoung.eom@gmail.com>
2017-07-18 11:50:14 +0900

    .

--- bit-java01/src/main/java/step14/Test02_6.java
+++ bit-java01/src/main/java/step14/Test02_6.java
@@ -1,4 +1,4 @@
-/* String 클래스 - chatAt()
+/* String 클래스 - chatAt(), 
  * => 특정 위치의 문자를 추출하기
  */
 package step14;
@@ -9,7 +9,11 @@ public class Test02_6 {
     String s1 = "abcdefghijklmn";
     char ch = s1.charAt(4); // 인덱스는 0부터 시작한다.
     System.out.println(ch);
+    System.out.println("-------------------------");
     
+    int i = s1.indexOf('f'); // 5
+    int i2 = s1.indexOf("cde"); // 2
+    System.out.printf("%d, %d\n", i, i2);
   }
 
 }
