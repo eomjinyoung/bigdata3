026e033c89f4f59c604bc468a02af92509824df9
jinyoung <jinyoung.eom@gmail.com>
2017-07-18 12:04:33 +0900

    .

--- bit-java01/src/main/java/step14/Test02_6.java
+++ bit-java01/src/main/java/step14/Test02_6.java
@@ -1,5 +1,5 @@
 /* String 클래스 - chatAt(), indexOf(), lastIndexOf(), startsWith(), endsWith(),
- *               toLowerCase(), toUpperCase()
+ *               toLowerCase(), toUpperCase(), trim()
  * => charAt(): 특정 위치의 문자를 추출하기
  * => indexOf(): 문자의 인덱스, 문자열의 시작 인덱스 
  *    lastIndexOf(): indexOf()랑 같다. 다만 문자열의 뒤쪽에서부터 찾는다.
@@ -7,6 +7,7 @@
  * => endsWith(): 문자열이 특정 문자열로 끝나는지 여부를 조사한다.
  * => toLowerCase(): 모든 문자를 소문자로 만든 새 String 인스턴스를 리턴한다.
  * => toUpplerCase(): 모든 문자를 대문자로 만든 새 String 인스턴스를 리턴한다.
+ * => trim(): 문자열의 시작과 끝에 있는 공백을 제거한다.
  */
 package step14;
 
@@ -30,9 +31,14 @@ public class Test02_6 {
     System.out.println("-------------------------");
     
     // 대소문자를 구분하지 않고 비교하는 방법
+    // => 문자열을 모두 대문자 또는 소문자로 바꾼 후에 비교하라! 
     String name2 = name.toLowerCase();
     if (name2.startsWith("james")) System.out.println("OK!");
     if (name2.endsWith("dean")) System.out.println("OK!");
+    System.out.println("-------------------------");
+    
+    String str = "    Hello,    홍길동!     ";
+    System.out.printf(">%s<\n", str.trim());
     System.out.println("-------------------------");
   }
 
