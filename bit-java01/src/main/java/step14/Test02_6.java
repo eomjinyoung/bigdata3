307e22e76c79b40c71620226d6f43f17c3ed66ff
jinyoung <jinyoung.eom@gmail.com>
2017-07-18 11:47:22 +0900

    .

---
+++ bit-java01/src/main/java/step14/Test02_6.java
@@ -0,0 +1,23 @@
+/* String 클래스 - chatAt()
+ * => 특정 위치의 문자를 추출하기
+ */
+package step14;
+
+public class Test02_6 {
+
+  public static void main(String[] args) {
+    String s1 = "abcdefghijklmn";
+    char ch = s1.charAt(4); // 인덱스는 0부터 시작한다.
+    System.out.println(ch);
+    
+  }
+
+}
+
+
+
+
+
+
+
+
