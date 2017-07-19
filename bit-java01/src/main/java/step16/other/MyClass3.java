ce4a5fb295cf631b87d9552f45785b5938e9eb4c
jinyoung <jinyoung.eom@gmail.com>
2017-07-19 17:19:20 +0900

    .

--- bit-java01/src/main/java/step16/other/MyClass3.java
+++ bit-java01/src/main/java/step16/other/MyClass3.java
@@ -5,6 +5,9 @@ package step16.other;
 public class MyClass3 {
   
   // m1()은 서브클래스에서 오버라이딩 할 수 없다.
+  // 오버라이딩을 못하게 하는 이유?
+  // => 원래의 기능 그대로 쓰도록 강제하고 싶을 때 사용하라!
+  // => 보안상 서브 클래스에서 재정의 하지 못하게 막고 싶을 때 사용하라! 
   public final void m1() {
     System.out.println("Hello!");
   }
