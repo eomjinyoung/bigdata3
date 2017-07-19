cee6e0a77539b5354c54f1553993368a3a2b7c56
jinyoung <jinyoung.eom@gmail.com>
2017-07-19 12:25:12 +0900

    .

--- bit-java01/src/main/java/step14/Test05_12.java
+++ bit-java01/src/main/java/step14/Test05_12.java
@@ -1,5 +1,10 @@
 /* 인스턴스 목록 다루기 : Java Collection API - Map 사용하기 V
- * => 사용자가 만든 클래스를 key로 사용할 수 있는가?
+ * => 사용자가 만든 클래스를 key로 사용하기
+ * => key 특징
+ *    - 인스턴스가 다르더라도 값이 같다면 같은 key로 취급되어야 한다.
+ *    - 그럴려면 key로 사용할 클래스는 반드시 hashCode()와 equals()를 오버라이딩 해야 한다.
+ *    - 만약 오버라이딩 하지 않는다면, 같은 값을 갖고 있어도 다른 key로 간주될 것이다.
+ *      즉 key로 사용할 수 없다는 뜻이다.
  *           
  */
 package step14;
@@ -28,29 +33,15 @@ public class Test05_12 {
   }
   
   public static void main(String[] args) {
-    HashMap studentMap = new HashMap();
+    MyKey k1 = new MyKey("한라산", "100");
+    MyKey k2 = new MyKey("한라산", "100");
     
-    studentMap.put("s01", new Student("홍길동", 20, false));
-    studentMap.put("s02", new Student("임꺽정", 30, true));
-    studentMap.put("s03", new Student("유관순", 16, false));
-    studentMap.put("s04", null);
-    studentMap.put(null, new Student("안중근", 20, true));
+    HashMap map = new HashMap();
     
-    System.out.println(studentMap.size());
-    System.out.println("----------------------------");
+    map.put(k1, "홍길동");
     
-    // Map에서 key와 value를 한쌍의 값으로 묶어 주머니에 담는다. 그리고 그 주머니를 리턴한다.
-    Set entrySet = studentMap.entrySet();
+    System.out.println(map.get(k1));
     
-    // Entry<key,value> : key와 value가 한 쌍으로 들어 있는 작은 주머니.
-    // Entry 집합에서 한 개씩 엔트리를 꺼내줄 대행자를 얻는다.
-    Iterator entryIterator = entrySet.iterator();
-    
-    // Iterator를 통해 주머니에서 entry를 한 개 꺼내 출력한다.
-    while (entryIterator.hasNext()) {
-      Entry entry = (Entry)entryIterator.next();
-      System.out.printf("%s:%s\n", entry.getKey(), entry.getValue()); 
-    }
   }
 }
 
