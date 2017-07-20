6b1108f3829f3f7be65f2bd6ead8d9754212abdb
jinyoung <jinyoung.eom@gmail.com>
2017-07-20 14:29:52 +0900

    .

---
+++ bit-java01/src/main/java/step15/ex3/other/Location.java
@@ -0,0 +1,19 @@
+package step15.ex3.other;
+
+import java.util.ArrayList;
+
+public class Location {
+  String name;
+  String tel;
+  ArrayList rooms;
+  
+  public Location(String name, String tel) {
+    super();
+    this.name = name;
+    this.tel = tel;
+    this.rooms = new ArrayList();
+  }
+  
+  
+  
+}
