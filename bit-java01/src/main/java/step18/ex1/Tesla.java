52fde2ccf7a4a5f1b15e4f89c4cba59d4c1ac916
jinyoung <jinyoung.eom@gmail.com>
2017-07-21 15:17:59 +0900

    .

--- bit-java01/src/main/java/step18/ex1/Tesla.java
+++ bit-java01/src/main/java/step18/ex1/Tesla.java
@@ -2,6 +2,7 @@ package step18.ex1;
 
 public class Tesla extends ElectMotor {
   int capacity;
+  int km;
   
   public void run() {
     System.out.println("스르륵 간다.");
--- bit-java01/src/main/java/step18/ex1/Test01.java
+++ bit-java01/src/main/java/step18/ex1/Test01.java
@@ -1,3 +1,6 @@
+/* 인터페이스 사용 전 
+ * => CarTester는 오직 Car 객체만 테스트할 수 있다.
+ */
 package step18.ex1;
 
 public class Test01 {
@@ -11,6 +14,25 @@ public class Test01 {
     CarTester.test(tico);
     CarTester.test(truck);
     
+    // 전기 자동차를 준비
+    Tesla tesla = new Tesla();
+    
+    // 자동차 시험소에서 전기 자동차르르 테스트 할 수 있을까?
+    // test(Car) 메서드는 오직 Car 객체만 파라미터로 받을 수 있다.
+    // Tesla 객체는 Car의 서브 클래스가 아니다. 
+    // 그래서 현재의 CarTester에서는 Tesla를 테스트 할 수 없다.
+    //CarTester.test(tesla); // 컴파일 오류!
+    
+    // 새로운 요구!
+    // => CarTester가 ElectCar를 상속 받은 전기 자동차도 테스트 하고 싶다!
+    // => 해결책? 다음 패키지를 보라!
   }
 
 }
+
+
+
+
+
+
+
