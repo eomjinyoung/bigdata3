7eda7595b0d93570d100f961027f80cf91b7e47f
jinyoung <jinyoung.eom@gmail.com>
2017-07-21 14:31:00 +0900

    .

---
+++ bit-java01/src/main/java/step18/ex1/Car.java
@@ -0,0 +1,36 @@
+package step18.ex1;
+
+public abstract class Car {
+  int capacity;
+  int fuelType;
+  int liter;
+  
+  public abstract void start();
+  public abstract void stop();
+  public abstract void run();
+  
+  public void breaking() {
+    System.out.println("속도를 줄인다.");
+  }
+  
+  public int getCapacity() {
+    return capacity;
+  }
+  public void setCapacity(int capacity) {
+    this.capacity = capacity;
+  }
+  public int getFuelType() {
+    return fuelType;
+  }
+  public void setFuelType(int fuelType) {
+    this.fuelType = fuelType;
+  }
+  public int getLiter() {
+    return liter;
+  }
+  public void setLiter(int liter) {
+    this.liter = liter;
+  }
+  
+  
+}
---
+++ bit-java01/src/main/java/step18/ex1/Tico.java
@@ -0,0 +1,11 @@
+package step18.ex1;
+
+public class Tico {
+  public void openSunroof() {
+    System.out.println("썬루프를 연다.");
+  }
+  
+  public void closeSunroot() {
+    System.out.println("썬루프를 닫는다.");
+  }
+}
