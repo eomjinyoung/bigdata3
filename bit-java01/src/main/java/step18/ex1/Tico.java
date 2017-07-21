acca11a92af63feae79641d6e78be228ac6a11d4
jinyoung <jinyoung.eom@gmail.com>
2017-07-21 14:33:23 +0900

    .

--- bit-java01/src/main/java/step18/ex1/Tico.java
+++ bit-java01/src/main/java/step18/ex1/Tico.java
@@ -1,6 +1,18 @@
 package step18.ex1;
 
-public class Tico {
+public class Tico extends Car {
+  public void start() {
+    System.out.println("모터를 돌려 시동을 건다.");    
+  }
+  
+  public void stop() {
+    System.out.println("연료를 끊어 시동을 끈다.");
+  }
+  
+  public void run() {
+    System.out.println("사방사방 달린다.");
+  }
+  
   public void openSunroof() {
     System.out.println("썬루프를 연다.");
   }
