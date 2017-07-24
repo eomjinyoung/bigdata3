6988e07e7a28ca54406e8b140845806f293f2675
jinyoung <jinyoung.eom@gmail.com>
2017-07-24 11:30:13 +0900

    .

--- bit-java01/src/main/java/step18/ex9/Test01.java
+++ bit-java01/src/main/java/step18/ex9/Test01.java
@@ -7,7 +7,9 @@ public class Test01 {
     System.out.println(A.a);
     System.out.println(A.b);
     
-    
+    // 인터페이스의 변수는 상수이기 때문에 값을 변경할 수 없다.
+    //A.a = 200; // 컴파일 오류!
+    //A.b = 300; // 컴파일 오류!
   }
 
 }
