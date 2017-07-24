1f72ac782a6c3717b2667ec3750b5c2d1b53ca7a
jinyoung <jinyoung.eom@gmail.com>
2017-07-24 09:54:19 +0900

    .

--- bit-java01/src/main/java/step18/ex5/Concrete2.java
+++ bit-java01/src/main/java/step18/ex5/Concrete2.java
@@ -1,17 +1,15 @@
 package step18.ex5;
 
-/* 보통 인터페이스 규칙에 따라 만든 일반 클래스를 
- * "콘크리트(concrete) 클래스"라 부른다.
- * 추상 클래스는 concrete 클래스라 부르지 않는다.
- * 인터페이스 구현하기
- * => class 클래스명 implements 규칙1, 규칙2, ... {
- *      규칙1, 규칙2 등에 선언된 모든 메서드를 구현한다.
- *    }
- * 
+/* 인터페이스의 선언된 메서드를 구현하지 않으면,
+ * 추상 메서드로 남게 된다.
+ * 일반 클래스는 추상 메서드를 가질 수 없기 때문에,
+ * 추상 클래스로 선언해야 한다.
  */ 
-public class Concrete2 implements A {
 
-  public void m1() {}
+public abstract class Concrete2 implements A {
+
+  //public void m1() {} // 이 메서드를 구현하지 않으면 추상 메서드로 남게 된다.
+                        // 따라서 클래스를 추상 클래스로 선언해야 한다.
   public void m2(int a) {}
   public void m3(String a, int b) {}
 
