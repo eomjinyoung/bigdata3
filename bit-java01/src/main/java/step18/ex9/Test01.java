fa746655512303615598764414241a4d161b8b18
jinyoung <jinyoung.eom@gmail.com>
2017-07-24 11:28:54 +0900

    .

---
+++ bit-java01/src/main/java/step18/ex9/Test01.java
@@ -0,0 +1,13 @@
+package step18.ex9;
+
+public class Test01 {
+
+  public static void main(String[] args) {
+    // 인터페이스의 상수 변수 사용하기
+    System.out.println(A.a);
+    System.out.println(A.b);
+    
+    
+  }
+
+}
