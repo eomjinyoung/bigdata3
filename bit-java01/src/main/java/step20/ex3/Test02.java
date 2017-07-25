221a2b061aae5db8a58f1f091d97ef248757a0c4
jinyoung <jinyoung.eom@gmail.com>
2017-07-25 11:10:46 +0900

    .

--- bit-java01/src/main/java/step20/ex3/Test02.java
+++ bit-java01/src/main/java/step20/ex3/Test02.java
@@ -1,10 +1,11 @@
-/* 애노테이션: 필수 속성
- * => 값을 반드시 지정해야 한다.
+/* 애노테이션: 선택 속성
+ * => 값을 지정하지 않으면 기본 값이 사용된다.
  */
 package step20.ex3;
 
-//@MyAnnotation() // 값을 지정하지 않으면 컴파일 오류이다.
-@MyAnnotation(name="홍길동", age=20)
+//@MyAnnotation2() // OK!
+//@MyAnnotation2 // OK! 단 한 개의 속성 값도 지정하지 않을 경우, 괄호()를 생략할 수 있다.
+@MyAnnotation2(gender=false) // OK! 선택 속성 중 필요한 것만 값을 지정할 수 있다.
 public class Test02 {
   public static void main(String[] args) {
     
