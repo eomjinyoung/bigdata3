6122e946281b2232343ba549c9be61b13a0ba5b2
jinyoung <jinyoung.eom@gmail.com>
2017-07-25 11:39:49 +0900

    .

--- bit-java01/src/main/java/step20/ex4/Test02.java
+++ bit-java01/src/main/java/step20/ex4/Test02.java
@@ -6,19 +6,19 @@ package step20.ex4;
 
 
 public class Test02 {
-  @MyAnnotation(value={"홍길동","임꺽정","유관순"}) // 배열 속성에 여러 개의 값 지정 
+  @MyAnnotation2(value={100, 90, 80}) // 배열 속성에 여러 개의 값 지정 
   void m1() {}
 
-  @MyAnnotation(value={"홍길동"}) // 배열 속성에 한 개의 값 지정 
+  @MyAnnotation2(value={100}) // 배열 속성에 한 개의 값 지정 
   void m2() {}
   
-  @MyAnnotation(value="홍길동") // 배열 속성에 한 개의 값만 지정할 경우 중괄호 생략 가능! 
+  @MyAnnotation2(value=100) // 배열 속성에 한 개의 값만 지정할 경우 중괄호 생략 가능! 
   void m3() {}
 
-  @MyAnnotation("홍길동") // 물론 배열 속성의 이름이 value이고, value 값만 지정할 경우 속성명 생략 가능! 
+  @MyAnnotation2(100) // 물론 배열 속성의 이름이 value이고, value 값만 지정할 경우 속성명 생략 가능! 
   void m4() {}
   
-  @MyAnnotation({"홍길동","임꺽정","유관순"})  
+  @MyAnnotation2({100,90,80})  
   void m5() {}
 }
 
