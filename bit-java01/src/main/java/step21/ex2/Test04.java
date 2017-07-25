af2dcbcac7ca6a2b70eafdaf58e9751f2187ca66
jinyoung <jinyoung.eom@gmail.com>
2017-07-25 15:33:57 +0900

    .

--- bit-java01/src/main/java/step21/ex2/Test04.java
+++ bit-java01/src/main/java/step21/ex2/Test04.java
@@ -10,17 +10,37 @@ public class Test04 {
   public static void main(String[] args) throws Exception {
     Class<?> clazz = Class.forName("step21.ex2.Truck");
     
-    // 특정 메서드만 추출할 수 있다.
-    Method method = clazz.getMethod("run");
+    // 파라미터가 있는 메서드 정보를 추출할 때는 메서드 이름 뿐만아니라 파라미터의 타입 정보도 줘야 한다.
+    // 주목!
+    // => 자바 원시 타입은 클래스가 아니지만, 원시 타입에 대한 정보를 추출할 때 예외적으로
+    //    일반 자바 클래스처럼 "class"라는 변수를 사용할 수 있다.
+    // => 예를 들면 int.class는 int의 타입을 가리킨다.
+    Method method = clazz.getMethod("setCapacity", int.class);
     
-    // 추출한 메서드 정보를 가지고 호출하기
-    // => 만약 추출한 메서드가 인스턴스 메서드라면 인스턴스 주소를 줘야지만 호출할 수 있다.
-    Truck obj = new Truck();
-    method.invoke(obj); // run() 메서드 호출하기
-                        // 이 메서드는 인스턴스 메서드이기 때문에
-                        // 호출할 때 invoke()의 파라미터로 인스턴스 주소를 줘야 한다.
+    // 호출할 때 파라미터를 넘겨야 하는 메서드 호출하기
+    // 먼저 인스턴스 메서드를 호출하려면 인스턴스가 필요하다.
+    Object obj = clazz.newInstance();
     
-    // 이렇게 메서드 정보를 통해 간접적으로 메서드를 호출할 수 있다.
+    // invoke()에 인스턴스 주소와 파라미터 값을 주면된다.
+    method.invoke(obj, 5);
+    
+    // 다른 인스턴스 메서드 호출하기 
+    method = clazz.getMethod("setFuelType", int.class);
+    method.invoke(obj, 1);
+    
+    // 또 다른 인스턴스 메서드 호출하기
+    method = clazz.getMethod("setLiter", int.class);
+    method.invoke(obj, 60);
+    
+    // toString() 메서드 호출하기
+    method = clazz.getMethod("toString");
+    String str = (String)method.invoke(obj); 
+    // 메서드를 호출할 때 파라미터가 없는 메서드는 인스턴스 주소만 주면 된다.
+    // 값을 리턴하는 메서드라면 변수로 받으면 된다.
+    
+    System.out.println(str);
+    
+    
   }
 
 }
