d58e645c64fd18663e6012e3ce15a84c4da9abc2
jinyoung <jinyoung.eom@gmail.com>
2017-07-25 16:40:43 +0900

    .

--- bit-java01/src/main/java/step22/ex4/Test01.java
+++ bit-java01/src/main/java/step22/ex4/Test01.java
@@ -10,33 +10,27 @@ public class Test01 {
     System.out.println(Calculator.minus(10, 20));
     System.out.println(Calculator.multiple(10, 20));
     
-    // 예외 처리 문법이 없던 시절에는
-    // 나누기를 할 때 항상 리턴 값을 확인해야 한다.
-    int result = Calculator.divide(20, 10);
-    if (result == -989898) 
-      System.out.println("0으로 나눌 수 없습니다.");
-    else
+    // 예외 처리 문법을 사용:
+    // 예외가 발생할 수 있는 메서드는 try 블록에서 실행하고,
+    // 예외 처리는 catch 블록에서 실행한다.
+    try {
+      int result = Calculator.divide(20, 10);
       System.out.println(result);
+      
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
     
-    result = Calculator.divide(20, 0);
-    if (result == -989898) 
-      System.out.println("0으로 나눌 수 없습니다.");
-    else
-      System.out.println(result);
-
+    try {
+      System.out.println(Calculator.divide(20, 0));
+      
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
     
-    // 리턴 값으로 예외 상황을 알리는 방법은
-    // 한 가지 문제를 안고 있다.
-    // 만약 리턴 값 자체가 유효한 나누기 결과 값이라면?
-    result = Calculator.divide(989898, -1);
-    if (result == -989898) 
-      System.out.println("0으로 나눌 수 없습니다.");
-    else
-      System.out.println(result);
+    // 위와 같이 메서드의 리턴 값이 아닌 별도의 경로로 예외 정보를 받기 때문에
+    // 정상적인 나누기가 수행되지 않을 가능성이 제거된다.
     
-    // 위 코드는 0으로 나누는 것이 아닌데도 결과가 "0으로 나눌 수 없습니다."를 출력한다.
-    // 즉 리턴 값으로 예외 상황을 알리는 방식은 항상 한계가 있다.
-    // 이런 것을 해결하기 위해 나온 문법이 예외 처리 문법이다.
     
     
   }
