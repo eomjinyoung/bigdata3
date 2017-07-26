7145f364e5ff201f0b24399f100c26655577fe9b
jinyoung <jinyoung.eom@gmail.com>
2017-07-26 16:36:48 +0900

    .

---
+++ bit-java01/src/main/java/step23/MyWebServer.java
@@ -0,0 +1,18 @@
+package step23;
+
+import java.net.ServerSocket;
+
+public class MyWebServer {
+
+  public static void main(String[] args) {
+    try {
+      ServerSocket ss = new ServerSocket(80);
+      
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+}
