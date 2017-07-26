7feea43bab2bffb8c66a2381d18a2d2c388b59f8
jinyoung <jinyoung.eom@gmail.com>
2017-07-26 12:23:51 +0900

    .

--- bit-java01/src/main/java/step22/ex5/Test04_2.java
+++ bit-java01/src/main/java/step22/ex5/Test04_2.java
@@ -1,13 +1,12 @@
-/* 예외 처리 문법: try ~ catch ~ 사용법
- * => 예외를 던지는 코드를 사용할 때 그 예외를 처리하는 문법이다.
+/* 예외 처리 문법: try ~ catch ~ 사용법 - 여러 타입을 한 개의 catch 블록에 통합하는 방법
+ * => Java 7에 추가된 문법이다.  
+ * => 오류를 처리하는 코드가 같을 때 각 예외 별로 중복해서 작성하는 것은 바람직하지 않다.
+ *    이런 경우 한 개의 catch 블록에서 처리하도록 한다.
  * => 문법
  *    try {
  *      예외를 던지는 코드
  *      ...
- *    } catch (예외를 받는 변수 선언) {
- *      예외를 처리하는 코드
- *      ...
- *    } catch (예외를 받는 변수 선언) {
+ *    } catch (예외타입1 | 예외타입2 | ... | 예외타입n 변수) {
  *      예외를 처리하는 코드
  *      ...
  *    }
@@ -16,7 +15,7 @@
  */
 package step22.ex5;
 
-public class Test04_1 {
+public class Test04_2 {
 
   static class EmptyStringException extends RuntimeException {
     public EmptyStringException() {}
@@ -42,11 +41,8 @@ public class Test04_1 {
       //m1(null);
       m1("");
       
-    } catch (NullPointerException e) {
-      System.out.println("null 값입니다."); 
-      
-    } catch (EmptyStringException e) {
-      System.out.println("빈 문자열입니다."); 
+    } catch (NullPointerException | EmptyStringException e) {
+      System.out.println("m1()의 파라미터 값이 null 또는 빈 문자열입니다."); 
     }
     
     // 또 다른 작업
