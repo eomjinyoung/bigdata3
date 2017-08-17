17da26bfe24296f299fef7fe1f5c4a5b44eade1c
jinyoung <jinyoung.eom@gmail.com>
2017-08-17 17:15:06 +0900

    .

--- bit-springboot01/v01/src/main/java/bigdata3/Application.java
+++ bit-springboot01/v01/src/main/java/bigdata3/Application.java
@@ -1,4 +1,14 @@
 /* 스프링 부트 실행과 배포: 
+ * [스프링 부트의 프로젝트 폴더 구조]
+ * ${프로젝트폴더}/src/main
+ *    /java     <== 자바 클래스 소스를 두는 폴더
+ *    /resources <== 기타 설정파일을 두는 폴더
+ *    /resources/static <== 정적 웹자원(.html, .css, .js, .gif 등)을 두는 폴더
+ * ${프로젝트폴더}/src/test
+ *    /java     <== 테스트 관련 자바 클래스 소스를 두는 폴더
+ *    /resources <== 테스트 관련 설정 파일을 두는 폴더
+ * ${프로젝트폴더}/build.gradle
+ * 
  * [스프링 부트 실행하기]
  * - 실행 명령
  *   ${프로젝트폴더}> gradle bootRun 
@@ -10,15 +20,6 @@
  *   => SpringApplication.run() 호출한다.
  *   => 임베디드 버전의 톰캣 서버를 실행한다.
  * 
- * - 스프링 부트의 프로젝트 폴더 구조
- * ${프로젝트폴더}/src/main
- *    /java     <== 자바 클래스 소스를 두는 폴더
- *    /resources <== 기타 설정파일을 두는 폴더
- *    /resources/static <== 정적 웹자원(.html, .css, .js, .gif 등)을 두는 폴더
- * ${프로젝트폴더}/src/test
- *    /java     <== 테스트 관련 자바 클래스 소스를 두는 폴더
- *    /resources <== 테스트 관련 설정 파일을 두는 폴더
- * ${프로젝트폴더}/build.gradle
  * 
  * [스프링 부트 배포하기]
  * 스프핑 부트 애플리케이션을 배포할 때 .jar 파일로 묶어 배포한다.
@@ -29,16 +30,16 @@
  *   > java -jar xxx.jar
  */
 
-package bigdata3.control;
+package bigdata3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @EnableAutoConfiguration
-public class Application01 {
+public class Application {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application01.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
