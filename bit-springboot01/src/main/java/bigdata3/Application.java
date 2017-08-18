170b92a18faa5a46a68440287d7a3b13a1717ebb
jinyoung <jinyoung.eom@gmail.com>
2017-08-18 12:29:42 +0900

    .

--- bit-springboot01/src/main/java/bigdata3/Application.java
+++ bit-springboot01/src/main/java/bigdata3/Application.java
@@ -1,53 +1,52 @@
-/* 기타 객체 생성하기 II
- * - 페이지 컨트롤러에서 사용할 기타 객체를 생성하는 방법
- *   => XML 설정
- *      외부에 스프링 관련 빈 설정 XML 파일을 만든다.
- *      @ImportResource 애노테이션을 이용하려 불러온다.
- *      
- *   => 애노테이션을 이용한 설정 
- *      @Bean 애노테이션을 이용하여 메서드에서 해당 객체를 리턴한다.
- *      
- * - 이 애노테이션을 사용하는 클래스에는 반드시 @Configuration 애노테이션을 붙여야 한다.
- *   왜? 스프링 프레임워크에게 다음을 알려주기 위함이다.
- *   "이 클래스에는 스프링 설정 정보를 포함하고 있습니다.
- *    그러니 이 클래스에 선언된 애노테이션을 잘분석하여 처리하세요!" 
- *    
- * - 자바 코드에서 직접 DataSource  객체를 만들 때 
- *   DBMS의 연결 정보를 지정해야 한다.
- *   방법1) 메서드를 호출하여 지정할 수 있고
- *   방법2) 스프링 부트 환경 변수를 사용하여 지정할 수 있다.
+/* 페이지 컨트롤러와 서비스 객체 준비
+ * - Spring MVC에서 사용할 JSP 엔진 준비 
+ *   => build.gradle에 톰캣 JSP 엔진 라이브러리 추가
+ *      compile("org.apache.tomcat.embed:tomcat-embed-jasper");
+ *   => build.gradle에 톰캣 JSTL 라이브러리 추가
+ *      compile group: 'javax.servlet', name: 'jstl', version: '1.2'
  *   
- * - 스프링 환경변수?
- *   루트 패키지에 application.properties 라는 이름의 파일에 등록한 변수이다.
- *   스프링 부트는 실행할 때 이 파일을 찾아서 그 내용을 메모리에 로딩된다.
- *    
+ * - 서비스 객체 준비
+ *   => bigdata3.service 패키지에 인터페이스 작성 
+ *   => bigdata3.service.impl 패키지에 인터페이스 구현체 작성
+ *   
+ * - 페이지 컨트롤러 준비
+ *   => bigdata3.web 패키지에 클래스 작성
+ *   
+ * - JSP 페이지 준비
+ *   => /webapp/WEB-INF/jsp/ 폴더에 JSP 작성
+ * 
+ * - HTML 파일 준비
+ *   => /resources/static 폴더에 HTML 작성
+ *   
+ * - JSP URL에 대해 접두사, 접미사 설정하기
+ *   => application.properties 파일에 다음 프로퍼티를 추가한다.
+ *      spring.mvc.view.prefix=/WEB-INF/jsp/
+ *      spring.mvc.view.suffix=.jsp
  */
 
 package bigdata3;
 
 import javax.sql.DataSource;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
+@EnableTransactionManagement // 트랜잭션 관리 객체 생성 
+@MapperScan("bigdata3.dao") // DAO 인터페이스로부터 클래스 자동 생성
 public class Application {
 
-  @Bean // 스프링 프레임워크는 @Bean이 붙은 메서드를 자동으로 호출한다.
-        // 메서드가 리턴한 객체를 Spring IoC 컨테이너에 보관한다.
+  @Bean 
   @ConfigurationProperties("app.datasource")
   public DataSource dataSource() {
-    System.out.println("===============> dataSource() 호출됨!"); 
     return DataSourceBuilder.create().build();
   }
- 
+
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Application.class, args);
   }
--- bit-springboot01/v08/src/main/java/bigdata3/web/TestController.java
+++ bit-springboot01/v08/src/main/java/bigdata3/web/TestController.java
@@ -10,15 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import bigdata3.dao.TeacherDao;
-
 @RestController
 public class TestController {
   @Autowired DataSource dataSource;
-  @Autowired TeacherDao teacherDao; // 만약 Mybatis가 TeacherDao를 구현한 클래스를 
-                                    // 만들지 않았다면, 이 인스턴스 변수에 
-                                    // 값을 주입하지 못하기 때문에 
-                                    // 스프링 부트를 실행할 때 오류가 발생할 것이다.
   
   @RequestMapping("/test1")
   public String test1() {
