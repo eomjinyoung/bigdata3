b27973075d5eaf376bd1f899173491f32461052f
jinyoung <jinyoung.eom@gmail.com>
2017-08-18 10:34:37 +0900

    .

--- bit-springboot01/src/main/java/bigdata3/web/TestController.java
+++ bit-springboot01/src/main/java/bigdata3/web/TestController.java
@@ -1,5 +1,7 @@
 package bigdata3.web;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.sql.Connection;
 
 import javax.sql.DataSource;
@@ -13,8 +15,15 @@ public class TestController {
   @Autowired DataSource dataSource;
   
   @RequestMapping("/test1")
-  public String test1() throws Exception {
-    Connection con = dataSource.getConnection();
-    return "ok!";
+  public String test1() {
+    try (Connection con = dataSource.getConnection();) {
+      return "ok!";
+      
+    } catch (Exception e) {
+      StringWriter out = new StringWriter(); // 문자열을 출력하는 도구. 출력한 문자열을 보관.
+      PrintWriter out2 = new PrintWriter(out); // 
+      e.printStackTrace(out2);
+      return out.toString(); // StringWriter에 보관된 내용을 꺼내 리턴한다.
+    }
   }
 }
