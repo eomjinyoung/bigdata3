1b9fe3c5df1845098c0f336b9d72b8733fdc754f
jinyoung <jinyoung.eom@gmail.com>
2017-08-21 15:33:52 +0900

    '.'

--- App01/app/src/main/java/bitcamp/bigdata3/app01/MainActivity.java
+++ App01/app/src/main/java/bitcamp/bigdata3/app01/MainActivity.java
@@ -1,20 +1,25 @@
-/* Intent를 이용하여 다른 앱을 실행하기
-=> Intent를 사용하여 안드로이드에게 명령을 전달할 수 있다.
-   예) 전화걸기 앱을 실행시켜라!
-   예) 웹브라우저를 띄워라!
-   예) 다른 액티비트를 실행하라!
-=> 안드로이드는 앱이 제출한 Intent를 검토하여
-   그에 맞는 가장 적절한 실행을 수행한다.
+/* View와 ViewGroup
+=> View 클래스?
+   - 안드로이드 UI 객체를 위한 공통 필드와 메서드를 정의한 클래스이다.
+   - 안드로이드의 모든 UI 객체는 이 클래스의 자손이다.
+   - UI 중에서 버튼이나 텍스트 상자와 같이
+     화면에 출력되는 View 객체를 "위젯(widget)"이라 부른다.
+   - 예) TextView, EditView, Button, ImageView 등
+
+=> ViewGroup 클래스?
+   - ViewGroup도 View의 자손이다.
+   - 단 다른 뷰를 담을 수 있는 기능을 갖고 있다.
+   - 실제 뭔가를 출력하는 것이 아니라 자신이 품고 있는 뷰 객체의
+     배치를 관리하는 일을 한다.
+   - 예) ConstraintLayout, LinearLayout, GridLayout
+
+=> 뷰 그룹을 중첩하여 원하는 UI를 만들 수 있다.
 
  */
 package bitcamp.bigdata3.app01;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,26 +29,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onButtonClick(View view) {
-        Toast msgBox = Toast.makeText(
-                this.getApplicationContext(), "눌렀음!", Toast.LENGTH_LONG);
-        msgBox.show();
-    }
-
-    public void onButton2Click(View view) {
-        Intent myIntent = new Intent(
-                Intent.ACTION_VIEW, Uri.parse("http://www.daum.net"));
-        this.startActivity(myIntent);
-    }
-
-    public void onButton3Click(View view) {
-        Intent myIntent = new Intent(
-                Intent.ACTION_VIEW, Uri.parse("tel:010-1111-2222"));
-        this.startActivity(myIntent);
-    }
-
-    public void onButton4Click(View view) {
-        Intent myIntent = new Intent(this.getApplicationContext(), SubActivity.class);
-        this.startActivity(myIntent);
-    }
 }
