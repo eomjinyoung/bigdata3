8a8feead9a61af6d51707e123da8646e97697334
jinyoung <jinyoung.eom@gmail.com>
2017-08-21 16:42:16 +0900

    '.'

---
+++ App01/app/src/main/java/bitcamp/bigdata3/app01/Button3Activity.java
@@ -0,0 +1,14 @@
+package bitcamp.bigdata3.app01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Button3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
--- App01/app/src05/main/java/bitcamp/bigdata3/app01/MainActivity.java
+++ App01/app/src05/main/java/bitcamp/bigdata3/app01/MainActivity.java
@@ -18,10 +18,8 @@
  */
 package bitcamp.bigdata3.app01;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,29 +27,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void onButton1Click(View v) {
-        // TextView 위젯을 자바 코드로 만들어 붙이기
-        Intent myIntent = new Intent(
-                this.getApplicationContext(), Button1Activity.class);
-        this.startActivity(myIntent);
-    }
-
-    public void onButton2Click(View v) {
-        // 레이아웃을 포함한 위젯을 자바 코드로 만들어 붙이기
-        Intent myIntent = new Intent(
-                this.getApplicationContext(), Button2Activity.class);
-        this.startActivity(myIntent);
-    }
-
-    public void onButton3Click(View v) {
-        // 자바코드 + XML = UI 만들기
-        // 레이아웃은 자바 코드로 만들고,
-        // TextView는 XML로 설계한다.
-        Intent myIntent = new Intent(
-                this.getApplicationContext(), Button3Activity.class);
-        this.startActivity(myIntent);
     }
 
 }
