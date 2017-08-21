393e4654e2aea979565aac9a38048442823313f9
jinyoung <jinyoung.eom@gmail.com>
2017-08-21 11:38:40 +0900

    '.'

--- App01/app/src/main/java/bitcamp/bigdata3/app01/MainActivity.java
+++ App01/app/src/main/java/bitcamp/bigdata3/app01/MainActivity.java
@@ -1,55 +1,34 @@
-/* 안드로이드 앱 빌드
-1) 자바 컴파일
-   .java ---> .class
-2) Dalvik VM Excutable Format
-   .class, .class, .class, ... ---> .dex
-   => 자바 바이트코드를 Dalvik VM이 이해할 수 있는 형식으로 변환한다.
-   => Dalvik?
-      - Java VM의 안드로이드 버전
-      - JVM은 라이센스 문제로 안드로이드 전용 VM을 만들게 되었다.
-3) Android Application Package Kit
-   .dex + AndroidManifest.xml + 기타 자원 ---> .apk
-   => 안드로이드 앱 설치 파일
-   => 플레이스토어나 USB를 통해 다운로드 받은 앱을 설치하면
-      내부적으로 안드로이드 폰은 이 파일을 지정된 폴더에 압축을 풀어 설치한다.
-
-[ART(Android Runtime)]
-=> Dalvik VM의 한계 및 라이센스 문제를 해결하기 위해 만든 VM이다.
-=> 안드로이드 5.0(Lolipop)에서 Dalvik을 대체하였다.
-
-[JIT(Just-in-Time)]
-=> 앱을 실행할 때 마다 바이트코드를 읽어서 처리한다면 매우느리다.
-   이런 문제를 해결하기 위해 나온 것이 JIT 컴파일 기법이다.
+/* 액티비티(Activity)
+=> 앱 화면을 담당하는 클래스이다.
+=> 실제 화면 설계는 별도의 XML 파일에 둔다.
+   액티비티는 화면의 출력을 제어하는 일을 한다.
+=> 그래서 보통 액티비티 한 개당 한 개 이상의 XML 파일이 연결된다.
 => 구동 원리
-   1) 앱을 실행할 때 마다
-   2) 일부의 바이트코드를 OS가 바로 인식할 수 있는 진짜 기계어로 변환한다.
-   3) 기계어로 변환된 것을 실행할 때는 JVM 없이 직접 실행하기 때문에 속도가 빠르다.
-=> 문제점
-   - 앱을 실행할 때 마다 변환하기 때문에 CPU에 부하가 걸린다.
-   - 실행 속도가 떨어진다.
-   - 밧데리 소모된다.
+   - 앱을 실행한다.
+   - AndroidManifest.xml(web.xml과 유사) 파일에 등록된 액티비트 중에서
+     최초 실행해야 하는 액티비티를 찾아 실행한다.
+   - 안드로이드는 해당 앱을 위한 빈 화면을 생성한다.
+   - 그리고 onCreate()를 호출하여 화면을 초기화할 기회를 준다.
+   - 액티비티에서는 화면 설계를 담고 있는 XML 파일의 아이디를 사용하여
+     XML 파일을 찾아 빈화면에 UI를 추가한다.
 
-[AOT(Ahead-of-Time)]
-=> JIT의 문제를 해결하기 위해 등장한 방식
-=> 구동원리
-   1) 안드로이드 앱을 설치한다.
-   2) 앱을 네이티브코드(OAT 포맷)로 변환한다.
-=> 문제점
-   - 앱을 설치하는 속도가 느리다.
-   - 미리 모든 앱을 네이티브로 변환하기 때문에 저장 용량을 많이 사용한다.
+[R 클래스]
+=> 안드로이드에서는 앱에서 사용하는 모든 자원(이미지 파일, XML, 기타)에 대해
+   아이디를 부여하고, 해당 아이디로 자원을 찾아 사용한다.
+   물론 assets/ 폴더에 파일을 두는 경우에는 아이디를 부여하지 않고
+   직접 사용한다.
+=> R 클래스는 자원에 대해 안드로이드 앱이 부여한 아이디 값을 저장하고 있는 클래스이다.
+=> 앱을 빌드할 때 자동으로 생성된다. 따라서 개발자가 직접 변경해서는 안된다.
+=> 아이디 이름은 보통 파일명을 참고하여 짓는다.
+   그래서 자원 파일을 만들 때는 반드시 파일명에 공백이 없어야 한다.
 
-[AOT + JIT]
-=> 안드로이드 7.0 부터 AOT에 JIT 방식을 결합하여 설치할 때 속도가 느린 점을 개선하였다.
-=> 구동원리
-   1) 안드로이드 앱을 설치한다.
-   2) 앱의 일부 코드를 네이티브 코드로 변환한다.
-   3) 실행할 때 또 일부 코드를 네이티브로 변환한다.
-   3) 그리고 핸드폰을 사용하지 않고 충전할 때 나머지 코드를 네이티브로 변환한다.
  */
 package bitcamp.bigdata3.app01;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,5 +36,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setContentView()
+        // => 액티비티의 빈 화면에 UI를 출력하는 일을 한다.
+        // 이 메서드의 파라미터 값으로 레이아웃 XML 파일의 아이디를 주면
+        // 이 메서드가 해당 XML 파일을 찾아
+        // 그 XML 파일에 설정된 대로 UI 객체를 만들고,
+        // 화면에 출력한다.
     }
 }
