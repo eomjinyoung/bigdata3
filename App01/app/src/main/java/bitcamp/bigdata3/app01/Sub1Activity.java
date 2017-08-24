b8084d50ead95b4e35897bbe887c97b2a60f2c62
jinyoung <jinyoung.eom@gmail.com>
2017-08-24 11:41:52 +0900

    .

--- App01/app/src/main/java/bitcamp/bigdata3/app01/Sub1Activity.java
+++ App01/app/src/main/java/bitcamp/bigdata3/app01/Sub1Activity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class OtherActivity extends AppCompatActivity {
+public class Sub1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_other);
+        setContentView(R.layout.activity_sub1);
     }
 
     public void onButton1Click(View v) {
