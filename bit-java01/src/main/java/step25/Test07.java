bf497a679e7b3aaa3cca8b505063a4962d286688
jinyoung <jinyoung.eom@gmail.com>
2017-08-25 11:18:02 +0900

    .

--- bit-java01/src/main/java/step25/Test07.java
+++ bit-java01/src/main/java/step25/Test07.java
@@ -1,5 +1,6 @@
 /* 스레드 - 자바의 기본 스레드들
- * 
+ * "main" 스레드그룹 
+ *   => "main" 스레드 : 실제 main()를 호출하는 스레드 
  *     
  */
 package step25;
@@ -11,6 +12,13 @@ public class Test07 {
     Thread t = Thread.currentThread();
     System.out.println(t.getName());
     
+    //=> "main" 스레드가 소속되어 있는 스레드 그룹
+    ThreadGroup g1 = t.getThreadGroup();
+    System.out.println(g1.getName());
+    
+    //=> "main" 스레드 그룹의 상위 그룹
+    ThreadGroup g2 = g1.getParent();
+    System.out.println(g2.getName());
   }
 }
 
