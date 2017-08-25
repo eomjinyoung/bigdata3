98fd3f252151e79cf1cca9c4a852f31d84657000
jinyoung <jinyoung.eom@gmail.com>
2017-08-25 11:33:46 +0900

    .

--- bit-java01/src/main/java/step25/Test07.java
+++ bit-java01/src/main/java/step25/Test07.java
@@ -2,7 +2,10 @@
  * "system" 스레드그룹 
  *   => "main" 스레드그룹 
  *      => "main" 스레드 : 실제 main()를 호출하는 스레드 
- *     
+ *   => "Reference Handler" 스레드 : 인스턴스를 가리키는 레퍼런스를 관리한다.
+ *   => "Finalizer" 스레드 : 가비지 인스턴스를 메모리에서 해제하는 일을 담당.
+ *   => "Signal Dispatcher" 스레드 : 키보드, 네트워크 이벤트를 관리
+ *   => "Attach Listener" 스레드  
  */
 package step25;
 
@@ -33,7 +36,29 @@ public class Test07 {
       System.out.println(groups[i].getName());
     }
     
+    //=> "system"에 속해있는 스레드들
+    System.out.println("-------------------------------");
+    Thread[] threads = new Thread[10];
+    count = g2.enumerate(threads, false); // 하위 그룹에 속해있는 스레드는 제외. 직속 스레드만!
+    for (int i = 0; i < count; i++) {
+      System.out.println(threads[i].getName());
+    }
     
+    //=> "main" 스레드 그룹의 하위 스레드 그룹 
+    System.out.println("-------------------------------");
+    count = g1.enumerate(groups);
+    for (int i = 0; i < count; i++) {
+      System.out.println(groups[i].getName());
+    }
+    // 아무것도 출력되지 않는다. 결론! "main" 그룹 안에 다른 그룹이 없다.
+    
+    //=> "main"에 속해있는 스레드들
+    System.out.println("-------------------------------");
+    count = g1.enumerate(threads, false);
+    for (int i = 0; i < count; i++) {
+      System.out.println(threads[i].getName());
+    }
+    // 결론! "main" 그룹 안에는 "main"이라는 스레드가 한 개만 있다.
     
   }
 }
