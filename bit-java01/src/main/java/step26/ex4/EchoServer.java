2e5691768d5577040174d37151d770147f043048
jinyoung <jinyoung.eom@gmail.com>
2017-08-28 09:42:44 +0900

    .

--- bit-java01/src/main/java/step26/ex4/EchoServer.java
+++ bit-java01/src/main/java/step26/ex4/EchoServer.java
@@ -1,54 +1,57 @@
-/* 서버 애플리케이션 만들기 III
- * => 스레드를 적용하여 여러 클라이언트 요청을 동시에 처리하라!
+/* 에코 서버 만들기
  *      
  */
 package step26.ex4;
 
-import java.io.PrintStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class EchoServer {
   
-  static class MessageAgent extends Thread {
+  static class EchoAgent extends Thread {
     Socket socket;
     
-    public MessageAgent(Socket socket) {
+    public EchoAgent(Socket socket) {
       this.socket = socket;
     }
     
     @Override
     public void run() {
-      try ( // java.lang.Autocloseable 구현체에 대해서만 변수를 선언할 수 있다.
-        Scanner in2 = new Scanner(socket.getInputStream());
-        PrintStream out2 = new PrintStream(socket.getOutputStream());) {
+      try ( 
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+      ) {
         
-        String message = in2.nextLine();
+        String message = in.readUTF();
         System.out.println(message);
-        out2.println("반갑습니다. 엄진영입니다.");
+        out.writeUTF(message);
         
       } catch (Exception e) {
-        // 일단 예외 상황을 무시한다.
+        e.printStackTrace();
+        
       } finally {
         try {socket.close();} catch (Exception e) {}
       }
     }
   }
   
-  public static void main(String[] args) throws Exception {
-    ServerSocket ss = new ServerSocket(9999);
-    System.out.println("클라이언트 연결을 기다리는 중...");
+  public void service() throws Exception {
+    try (
+      ServerSocket ss = new ServerSocket(9999);
+    ) {
+      System.out.println("클라이언트 연결을 기다리는 중...");
     
-    while (true) {
-      //Socket socket = ss.accept();
-      //MessageAgent agent = new MessageAgent(socket);
-      //agent.start();
-      new MessageAgent(ss.accept()).start();
+      while (true) {
+        new EchoAgent(ss.accept()).start();
+      }
     }
-    
-    //ss.close();
-    
+  }
+  
+  public static void main(String[] args) throws Exception {
+    EchoServer echoServer = new EchoServer();
+    echoServer.service();
   }
 }
 
