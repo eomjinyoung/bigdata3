fb93f9ddcabaabb1dce8c64c279ab06c1461588c
jinyoung <jinyoung.eom@gmail.com>
2017-08-28 12:40:14 +0900

    .

---
+++ bit-java01/src/main/java/step15/ex4/Test01_4.java
@@ -0,0 +1,21 @@
+/* local class - 인스턴스 변수 접근
+ *     
+ */
+package step15.ex4;
+
+public class Test01_4 {
+  
+  public void test1() {
+    
+  }
+  
+  public static void main(String[] args) {
+    Test01_4 obj = new Test01_4();
+    obj.test1();
+  }
+}
+
+
+
+
+
