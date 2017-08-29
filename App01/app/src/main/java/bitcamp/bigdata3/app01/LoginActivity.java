c515b9a07c4200243b14d4e84446f82036900082
jinyoung <jinyoung.eom@gmail.com>
2017-08-29 15:37:59 +0900

    .

--- App01/app/src/main/java/bitcamp/bigdata3/app01/LoginActivity.java
+++ App01/app/src/main/java/bitcamp/bigdata3/app01/LoginActivity.java
@@ -7,7 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
@@ -32,16 +38,16 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
             try {
-                URL url = new URL("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1");
+                URL url = new URL(String.format(
+                        "http://192.168.0.6:8080/auth/json/login?email=%s&password=%s",
+                        params[0], params[1]));
                 URLConnection urlConnection = url.openConnection();
 
                 Scanner in = new Scanner(urlConnection.getInputStream());
                 StringBuffer buf = new StringBuffer();
-
                 while (in.hasNext()) {
                     buf.append(in.nextLine());
                 }
-
                 return buf.toString();
 
             } catch (Exception e) {
@@ -52,8 +58,33 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String result) {
-            Log.v(TAG, result);
+        protected void onPostExecute(String json) {
+            try {
+                if (json == null)
+                    throw new Exception("서버 요청 오류!");
+
+                JSONObject jsonObject = new JSONObject(json); // 문자열 ==> 자바 객체
+                Log.v(TAG, jsonObject.getString("state"));
+
+                JSONObject member = jsonObject.getJSONObject("data");
+                Log.v(TAG, member.getString("name"));
+
+                JSONArray photoList = member.getJSONArray("photoList");
+                for (int i = 0; i < photoList.length(); i++) {
+                    Log.v(TAG, photoList.getString(i));
+                }
+
+                Toast.makeText(LoginActivity.this, "로그인 성공!", Toast.LENGTH_LONG).show();
+
+            } catch (Exception e) {
+                Toast.makeText(LoginActivity.this,
+                        "로그인 오류!", Toast.LENGTH_LONG).show();
+
+                // 어떤 오류인지 자세한 내용은 로그로 출력한다.
+                StringWriter out = new StringWriter();
+                e.printStackTrace(new PrintWriter(out));
+                Log.e(TAG, out.toString());
+            }
             LoginActivity.this.finish();
         }
     }
