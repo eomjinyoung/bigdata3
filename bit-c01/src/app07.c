bd7be6d2827c34f547d43bfa9374088826c8e6ad
jinyoung <jinyoung.eom@gmail.com>
2017-08-31 15:31:41 +0900

    .

--- bit-c01/src/app07.c
+++ bit-c01/src/app07.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int main07(void) {
 	int scores[5];
 	int a = 100;
 
---
+++ bit-c01/src/app12.c
@@ -0,0 +1,21 @@
+/* 변수와 데이터형 - 포인터 변수 II
+ * => 포인터 변수의 다양한 선언
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main11(void) {
+
+	return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
