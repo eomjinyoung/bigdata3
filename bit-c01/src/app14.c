502da91849108b3be6967f07fd1bfe1def4d0573
jinyoung <jinyoung.eom@gmail.com>
2017-08-31 16:58:30 +0900

    .

--- bit-c01/src/app14.c
+++ bit-c01/src/app14.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void) {
+int main14(void) {
 	int a1[] = {101, 102, 103, 104, 105};
 
 	int *p = a1;
@@ -20,8 +20,24 @@ int main(void) {
 	printf("\n");
 
 	// 포인터의 주소를 계산하여 배열의 항목을 찾기
+	// => int 타입일 경우 주소에 더하는 단위는 4바이트이다.
 	for (int i = 0; i < 5; i++) {
-		printf("%d,", *(p + i));
+		printf("%d,", *(p + i)); // 즉 p에 저장된 주소에 4바이트 단위로 더한다.
+	}
+	printf("\n");
+
+
+	// 다른 타입의 예
+	// => char 타입일 경우 주소에 더하는 단위는 1바이트이다.
+	char str[] = "Hello";
+	char *p2 = str;
+	for (int i = 0; i < strlen(str); i++) {
+		printf("%c,", p2[i]);
+	}
+	printf("\n");
+
+	for (int i = 0; i < strlen(str); i++) {
+		printf("%c,", *(p2 + i));
 	}
 	printf("\n");
 
