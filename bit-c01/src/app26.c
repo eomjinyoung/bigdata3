9a63fbbe4508595d79368119e9732d8f3025df32
jinyoung <jinyoung.eom@gmail.com>
2017-09-01 17:23:24 +0900

    .

--- bit-c01/src/app26.c
+++ bit-c01/src/app26.c
@@ -1,36 +1,28 @@
-/* 변수의 사용 범위 제어
- * => static : 함수 안에서만 사용되는 글로벌 변수
+/* 변수의 사용 범위 제어 II
+ * => 글로벌 변수 :
+ *    - .c 파일에서 함수 바깥에 선언된 변수
+ *    - 이 변수는 선언된 파일에서만 접근할 수 있다.
+ *
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// 로컬 변수
-// => 함수가 호출될 때 생성되고, 호출이 끝나면 제거되는 변수
-void app25_f1(int value) {
-	int sum = 0;
-	sum += value;
-	printf("합계=%d\n", sum);
-}
+int app26_sum = 0;
 
-// 스태틱 변수
-// => 함수가 호출될 때 생성되고, 한 번 생성된 변수는 프로그램을 종료할 때까지 계속 유지된다.
-// => 그 함수에서만 사용되는 글로벌 변수라 할 수 있다.
-void app25_f2(int value) {
-	static int sum = 0; // 처음 호출될 때 sum은 생성되고 0으로 초기화 된다.
-						// 그 이후에 호출될 때는 실행되지 않는다.
-	sum += value;
-	printf("합계=%d\n", sum);
-}
+// 글로벌 변수를 사용하는 함수
+void app26_plus(int value) {app26_sum += value;}
+void app26_minus(int value) {app26_sum -= value;}
 
-int main25(void) {
-	app25_f1(100);
-	app25_f1(50);
-	puts("----------------------------");
+int main(void) {
+	app26_plus(100);
+	app26_plus(27);
+	app26_plus(37);
+	app26_minus(5);
+	app26_minus(6);
 
-	app25_f2(100);
-	app25_f2(50);
+	printf("합계=%d\n", app26_sum);
 
 	return EXIT_SUCCESS;
 }
