b369820e3ee0e14a54fe96beabad399e9359a8ce
jinyoung <jinyoung.eom@gmail.com>
2017-09-04 16:37:19 +0900

    .

--- bit-c01/src/app65.c
+++ bit-c01/src/app65.c
@@ -6,14 +6,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void) {
+int main65(void) {
 	char name[50];
-	int age;
+	int kor, eng, math, sum;
+	float aver;
 
-	printf("name age?");
-	scanf("%s %d", name, &age);
+	printf("name kor eng math?");
+	scanf("%s %d %d %d", name, &kor, &eng, &math);
 
-	printf("%s, %d\n", name, age);
+	sum = kor + eng + math;
+	aver = sum / 3.0;
+
+	printf("name: %s\n", name);
+	printf("total: %d\n", sum);
+	printf("average: %f\n", aver);
 
 	return EXIT_SUCCESS;
 }
