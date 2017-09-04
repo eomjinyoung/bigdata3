dfaae7937b2d133a3b888cc849c6bdf8510c1333
jinyoung <jinyoung.eom@gmail.com>
2017-09-04 11:02:04 +0900

    .

--- bit-c01/src/app38.c
+++ bit-c01/src/app38.c
@@ -1,21 +1,27 @@
-/* 조건문 - if ~ else if ~ else if ~ else
+/* 조건문 - else의 소속
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main37(void) {
-	int age = 18;
+int main38(void) {
+	int age = 9;
 
-	if (age < 8)
-		printf("아동\n");
-	else if (age >= 8 && age < 14)
+	if (age > 13)
+		if (age > 18)
+			printf("성인\n");
+	else // else는 항상 가장 가까이에 있는 if와 묶인다. 따라서 이 else 문은 바로 전 if (age > 1*)문과 묶인다.
 		printf("어린이\n");
-	else if (age >= 14 && age < 19)
-		printf("청소년\n");
+
+	printf("--------------------------\n");
+
+	if (age > 13) {
+		if (age > 18)
+			printf("성인\n");
+	}
 	else
-		printf("성년\n");
+		printf("어린이\n");
 
 	return EXIT_SUCCESS;
 }
