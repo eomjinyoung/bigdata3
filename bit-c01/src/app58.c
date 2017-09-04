926e5bd5f2ea03acd93ff6e4db8b6b6da6f84eed
jinyoung <jinyoung.eom@gmail.com>
2017-09-04 14:37:14 +0900

    .

--- bit-c01/src/app58.c
+++ bit-c01/src/app58.c
@@ -1,32 +1,40 @@
-/* 구조체 - typedef III
- * => 함수 포인터의 typedef
+/* 구조체 - union
+ * => 다양한 타입의 멤버를 한 개만 유지하고 싶을 때 사용한다.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int app57_plus(int a, int b) {return a + b;}
-int app57_minus(int a, int b) {return a - b;}
-int app57_multiple(int a, int b) {return a * b;}
-int app57_divide(int a, int b) {return a / b;}
+int main58(void) {
 
-// 함수 포인터 타입을 선언하는 방법
-typedef int (*Func)(int, int);
+	// union 적용 전
+	char name[20];
+	int age;
 
-int main57(void) {
+	// 다음 세 변수는 저장하려는 데이터에 따라 사용여부가 결정된다.
+	// => 그럼에도 불구하고 일단 다음 세 개의 변수를 모두 준비해야 한다.
+	int grade;
+	float score;
+	char working;
 
-	// 함수 포인터를 사용하여 함수 주소를 받기
-	int (*f)(int, int); // OK!
-	Func f2;
+	// 학생 정보를 저장
+	strcpy(name, "홍길동");
+	age = 20;
+	grade = 4;
+	printf("%s, %d, %d\n", name, age, grade);
 
-	// 함수 주소 저장
-	f = app57_multiple;
-	f2 = app57_minus;
+	// 졸업생 정보를 저장
+	strcpy(name, "임꺽정");
+	age = 30;
+	score = 3.7;
+	printf("%s, %d, %f\n", name, age, score);
 
-	// 함수 호출
-	printf("%d\n", f(5, 7));
-	printf("%d\n", f2(5, 7));
+	// 직장인 정보를 저장
+	strcpy(name, "유관순");
+	age = 22;
+	working = 'Y';
+	printf("%s, %d, %c\n", name, age, working);
 
 	return EXIT_SUCCESS;
 }
