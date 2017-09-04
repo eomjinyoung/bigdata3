0e2d98dcbcc0ee54073a17d47ca5ecb07b51b31f
jinyoung <jinyoung.eom@gmail.com>
2017-09-04 14:15:27 +0900

    .

---
+++ bit-c01/src/app57.c
@@ -0,0 +1,25 @@
+/* 구조체 - typedef III
+ * => 함수 포인터의 typedef
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int plus(int a, int b) {return a + b;}
+int minus(int a, int b) {return a - b;}
+int multiple(int a, int b) {return a * b;}
+int divide(int a, int b) {return a / b;}
+
+int main(void) {
+
+
+	return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
