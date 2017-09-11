d984b50848436ff8fbc051598fafca9ba0027a86
jinyoung <jinyoung.eom@gmail.com>
2017-09-11 15:57:24 +0900

    .

--- App01/app/src/main/java/bitcamp/bigdata3/app01/MainActivity.java
+++ App01/app/src/main/java/bitcamp/bigdata3/app01/MainActivity.java
@@ -10,14 +10,19 @@
  */
 package bitcamp.bigdata3.app01;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getName();
+    MyService3 myService3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,8 +87,45 @@ public class MainActivity extends AppCompatActivity {
 
         // MainActivity가 종료되었다 하더라도 MyService2는 밑단(background)에서
         // 계속 실행할 것이다.
-        this.finish();
+        //this.finish();
     }
 
+    public void onButton4Click(View v) {
+        // 서비스 멈추기
+        Intent intent = new Intent(getApplicationContext(), MyService2.class);
+        stopService(intent);
+    }
+
+    public void onButton5Click(View v) {
+        Intent intent = new Intent(getApplicationContext(), MyService3.class);
+        // 1번 파라미터: 생성하고 실행할 서비스 클래스 정보
+        // 2번 파라미터: 안드로이드 플랫폼에서 서비스를 생성한 후 그 서비스 객체의
+        //              주소를 받기 위해 파견 보내는 놈!
+        //              파견 보내는 놈은 반드시 ServiceConnection 구현체이어야 한다.
+        // 3번 파라미터: 바인더 객체를 자동 생성하도록 설정
+        bindService(intent, new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                Log.d(TAG, "onServiceConnected()");
+                MyService3.LocalBinder binder = (MyService3.LocalBinder) service;
+                myService3 = binder.getService();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                Log.d(TAG, "onServiceDisconnected()");
+            }
+        }, Context.BIND_AUTO_CREATE);
+    }
+
+    public void onButton6Click(View v) {
+        // bindService()를 호출할 때 ServiceConnection을 파견했었다.
+        // 안드로이드 플랫폼이 서비스를 만든 후에
+        // 파견자 ServiceConnection에게 IBinder를 알려 줬다.
+        // 파견자 ServiceConnection은 IBinder를 통해 서비스 객체를 알아내서
+        // 액티비티의 변수에 저장하였다.
+        // 지금 그 변수를 이용하여 서비스 객체의 메서드를 호출하는 것이다.
+        Log.d(TAG, "getValue() ==> " + myService3.getValue());
+    }
 
 }
