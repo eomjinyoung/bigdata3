ad6460d3c15ca5fcf9285af1d47f4312f0ba3546
jinyoung <jinyoung.eom@gmail.com>
2017-09-12 19:04:18 +0900

    .

--- bit-docs/iot_data.sql
+++ bit-docs/iot_data.sql
@@ -1,12 +1,12 @@
-insert into iot_user(email,name,pwd) values('user1@test.com', '홍길동', '1111');
-insert into iot_user(email,name,pwd) values('user2@test.com', '홍길동2', '1111');
-insert into iot_user(email,name,pwd) values('user3@test.com', '홍길동3', '1111');
-insert into iot_user(email,name,pwd) values('user4@test.com', '홍길동4', '1111');
-insert into iot_user(email,name,pwd) values('user5@test.com', '홍길동5', '1111');
-insert into iot_user(email,name,pwd) values('user6@test.com', '홍길동6', '1111');
-insert into iot_user(email,name,pwd) values('user7@test.com', '홍길동7', '1111');
-insert into iot_user(email,name,pwd) values('user8@test.com', '홍길동8', '1111');
-insert into iot_user(email,name,pwd) values('user9@test.com', '홍길동9', '1111');
-insert into iot_user(email,name,pwd) values('user10@test.com', '홍길동10', '1111');
-insert into iot_user(email,name,pwd) values('user11@test.com', '홍길동11', '1111');
+insert into iot_user(email,name,pwd) values('user1@test.com', '홍길동', password('1111'));
+insert into iot_user(email,name,pwd) values('user2@test.com', '홍길동2', password('1111'));
+insert into iot_user(email,name,pwd) values('user3@test.com', '홍길동3', password('1111'));
+insert into iot_user(email,name,pwd) values('user4@test.com', '홍길동4', password('1111'));
+insert into iot_user(email,name,pwd) values('user5@test.com', '홍길동5', password('1111'));
+insert into iot_user(email,name,pwd) values('user6@test.com', '홍길동6', password('1111'));
+insert into iot_user(email,name,pwd) values('user7@test.com', '홍길동7', password('1111'));
+insert into iot_user(email,name,pwd) values('user8@test.com', '홍길동8', password('1111'));
+insert into iot_user(email,name,pwd) values('user9@test.com', '홍길동9', password('1111'));
+insert into iot_user(email,name,pwd) values('user10@test.com', '홍길동10', password('1111'));
+insert into iot_user(email,name,pwd) values('user11@test.com', '홍길동11', password('1111'));
 
