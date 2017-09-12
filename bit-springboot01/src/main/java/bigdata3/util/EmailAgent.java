00d1d05fb2ed0ec56a93171637af07f0c90b5d7a
jinyoung <jinyoung.eom@gmail.com>
2017-09-12 14:51:30 +0900

    .

--- bit-springboot01/src/main/java/bigdata3/util/EmailAgent.java
+++ bit-springboot01/src/main/java/bigdata3/util/EmailAgent.java
@@ -1,13 +1,13 @@
 package bigdata3.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailAgent {
-  @Autowired MailSender mailSender;
+  @Autowired JavaMailSender mailSender;
   
   public void send(String to, String subject, String text) {
     SimpleMailMessage message = new SimpleMailMessage();
@@ -17,5 +17,11 @@ public class EmailAgent {
     message.setText(text);
     
     mailSender.send(message);
+    
+    /* 주의 사항
+     * => 구글의 경우 기본적으로 보안수준이 낮은 앱에 대해 접근을 허용하지 않는다.
+     *    임시적으로 풀고 싶다면 다음 사이트로 접속하여 보안 수준을 조정하라!
+     * => https://www.google.com/settings/security/lesssecureapps
+     */
   }
 }
