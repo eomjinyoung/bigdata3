d36f9285a45097c192b6c4d1fbc42c4c884e3d3a
jinyoung <jinyoung.eom@gmail.com>
2017-09-13 21:05:59 +0900

    '.'

--- bit-arduino/sketch06/sketch06.ino
+++ bit-arduino/sketch06/sketch06.ino
@@ -1,5 +1,7 @@
 int buttonPin = 2;
 int ledPin = 4;
+boolean buttonPressed = false;
+boolean reset = false;
 
 void setup() {
   Serial.begin(9600);  
@@ -13,6 +15,13 @@ void loop() {
   detectButtonSignal(currMillis);
   toggleLed(currMillis);
 
+  if (Serial.available()) {
+    String cmd = Serial.readString();
+    if (cmd == "reset") {
+      Serial.println(reset);
+      reset = false;
+    }
+  }
   delay(10);
 }
 
@@ -20,18 +29,20 @@ void detectButtonSignal(unsigned long currMillis) {
   static int count = 0;
   static unsigned long savedMillis = 0;
   
-  if (currMillis < savedMillis + 1000)
+  if (reset || currMillis < savedMillis + 1000)
     return;
 
   savedMillis = currMillis;
-  Serial.println(count);
   if (digitalRead(buttonPin) == HIGH) {
     count++;
+    buttonPressed = true;
   } else {
     count = 0;
+    buttonPressed = false;
   }
   if (count > 5) {
-    Serial.println("do reset");
+    reset = true;
+    buttonPressed = false;
   }  
 }
 
@@ -43,7 +54,7 @@ void toggleLed(unsigned long currMillis) {
     return;
     
   savedMillis = currMillis;  
-  if (state == LOW) {
+  if (state == LOW && buttonPressed) {
     state = HIGH;
   } else {
     state = LOW;
--- bit-python01/src/app.py
+++ bit-python01/src/app.py
@@ -1,8 +1,17 @@
+import serial
+import time
 import iot
 
 serialNo = "192.168.0.6"
+ser = serial.Serial('COM6') # 시리얼 포트를 준비한다.
+time.sleep(2) # 장비와 연결될 시간을 확보한다.
 
-result = iot.reset(serialNo)
+while True:
+    ser.write(b"reset")
+    result = str(ser.readline(), "utf-8").rstrip()
+    print(result)
+    time.sleep(5)
 
-print(result)
 
+#result = iot.reset(serialNo)
+#print(result)
