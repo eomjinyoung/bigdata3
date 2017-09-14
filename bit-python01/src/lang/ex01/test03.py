9ef43fd8f55e4f2e85f3a7d1bbac249d87c1cbd6
jinyoung <jinyoung.eom@gmail.com>
2017-09-14 21:08:34 +0900

    .

--- bit-python01/src/lang/ex01/test03.py
+++ bit-python01/src/lang/ex01/test03.py
@@ -4,17 +4,19 @@ from math import inf
 # 문자열 표현
 print("Hello")
 print('Hello')
-print("Hello,\tworld!")
-print("Hello,\nworld!")
 
-# 문자열 형식 정의
-print("Hello, {name}")
+# 작은 따옴표 또는 큰 따옴표 한 개를 사용하면 문자열을 여러 줄 작성할 수 없다.
+#print('안녕하세요,
+#반값습니다.
+#또 오세요!')
 
-name = "홍길동"
-print(f"Hello, {name}")
+# 문자열을 여러 줄 작성하고 싶다면 따옴표(' or ")를 3 개 사용하라!
+print('''안녕하세요,
+반값습니다.
+또 오세요!''') # 여러 줄 작성이 가능하다.
 
-age = 20
-print(f"{name} 나이 = {age}")
+
+print("--------------------------------")
 
 # 정수값 표현
 print(10)       # 10진수
@@ -29,6 +31,8 @@ print(0X17)
 print(123_456) # _의 위치는 자유롭다
 print(12_3456)
 print(0b0110_0001) # 97
+
+print("--------------------------------")
 
 # 부동 소수점값 표현
 print(3.14)
@@ -45,13 +49,19 @@ print(0.314E1)
 print(inf) # + 무한대, 모듈 import가 필요하다.
 print(-inf) # - 무한대
 
+print("--------------------------------")
+
 # 논리값 표현
 print(True)
 print(False)
 #print(true) # 실행 오류!
 #print(false) # 실행 오류!
 
+print("--------------------------------")
 
+# 객체를 가리키고 있지 않음을 표현하는 값
+# -자바의 null과 같은 의미
+print(None)
 
 
 
