1317fdf2fbdf0d06d1bfe90311cfb9f0bcb7ef35
jinyoung <jinyoung.eom@gmail.com>
2017-09-15 16:12:19 +0900

    .

---
+++ bit-python01/src/test02.py
@@ -0,0 +1,12 @@
+# 모듈 사용하기
+
+# calculator.py 모듈 가져오기
+# => import 모듈명
+import calculator
+ 
+# 모듈에 있는 함수 호출하기
+# => 모듈명.함수명
+print(calculator.plus(10, 20))
+print(calculator.minus(10, 20))
+print(calculator.multiple(10, 20))
+print(calculator.divide(10, 20))
