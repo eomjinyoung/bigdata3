5ab3e3cf6f857b6ff6806027ec861af0bfb7741c
jinyoung <jinyoung.eom@gmail.com>
2017-09-15 15:25:48 +0900

    .

--- bit-python01/src/lang/ex02/test03.py
+++ bit-python01/src/lang/ex02/test03.py
@@ -1,24 +1,42 @@
-# 함수 - call by value 와 call by reference
+# 함수 - call by value 와 call by reference II
 # - immutable 값을 넘기면 call by value 가 된다.
 # - mutable 값을 넘기면 call by reference 가 된다.
 
-# call by value
+# call by reference
 def f1(a):
-    a = 100;
+    a[0] = 100;
 
-v1 = 1
-v2 = 3.14
-v3 = "홍길동"
-v4 = True
+l1 = [1, 2, 3]
+f1(l1) # 리스트의 주소가 넘어간다.
+print(l1)
 
-f1(v1)
-print(v1)
+t1 = (1, 2, 3)
+#f1(t1) # 튜플의 주소가 넘어간다.
+       # 그러나 튜플의 값을 변경할 수 없기 때문에
+       # 오류 발생!
+       
+print("-----------------------")
 
-f1(v2)
-print(v2)
+def f2(d):
+    d['tel'] = '1111-2222'
 
-f1(v3)
-print(v3)
+d1 = {
+    'name': '홍길동',
+    'age': 20,
+    'working': True,
+    }
+f2(d1) # 딕셔너리의 주소를 넘긴다.
+print(d1)
+    
+print("-----------------------")
 
-f1(v4)
-print(v4)
+def f3():
+    return {'name': '홍길동', 'age': 20}
+
+d2 = f3()
+print(d2)
+    
+    
+    
+    
+    
