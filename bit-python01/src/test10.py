34c27d36314b82cca209a9ae3e8e505791ced04a
jinyoung <jinyoung.eom@gmail.com>
2017-09-15 18:06:07 +0900

    .

---
+++ bit-python01/src/test10.py
@@ -0,0 +1,9 @@
+# 클래스 사용하기 - 클래스의 스태틱 메서드 호출하기
+
+# => from 패키지명.모듈명 import 클래스명
+from ex04.calculator4 import Calculator
+
+print(Calculator.plus(10, 20))
+print(Calculator.minus(10, 20))
+print(Calculator.multiple(10, 20))
+print(Calculator.divide(10, 20))
